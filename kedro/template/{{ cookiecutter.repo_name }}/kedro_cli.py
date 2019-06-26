869c33d7f07894695714873093e8e7abeee34b3f
Nikos Tsaousis <tsanikgr@users.noreply.github.com>
2019-06-26 09:52:09 +0100

    Ensure local code changes take precedence over installed packages (#99)

--- kedro/template/{{ cookiecutter.repo_name }}/kedro_cli.py
+++ kedro/template/{{ cookiecutter.repo_name }}/kedro_cli.py
@@ -44,9 +44,9 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 # get our package onto the python path
 PROJ_PATH = Path(__file__).resolve().parent
-sys.path.append(str(PROJ_PATH / "src"))
+sys.path.insert(0, str(PROJ_PATH / "src"))
 os.environ["PYTHONPATH"] = (
-    os.environ.get("PYTHONPATH", "") + os.pathsep + str(PROJ_PATH / "src")
+    str(PROJ_PATH / "src") + os.pathsep + os.environ.get("PYTHONPATH", "")
 )
 os.environ["IPYTHONDIR"] = str(PROJ_PATH / ".ipython")
 

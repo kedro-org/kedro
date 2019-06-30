916d203dd8cd7169ca707f9e10685069a056dfca
Joost Duisters <j.h.duisters@gmail.com>
2019-06-30 18:26:52 +0100

    Correct type hinting for *patterns argument

--- kedro/config/config.py
+++ kedro/config/config.py
@@ -123,7 +123,7 @@ class ConfigLoader:
         self.conf_paths = conf_paths
         self.logger = logging.getLogger(__name__)
 
-    def get(self, *patterns: str) -> Dict[str, Any]:
+    def get(self, *patterns: Tuple[str]) -> Dict[str, Any]:
         """Recursively scan for configuration files, load and merge them, and
         return them in the form of a config dictionary.
 

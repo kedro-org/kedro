881bfa97a4e637b9c74c73dbb058e6ffde2dab40
mdomar <omar_saleem_mohammed@mckinsey.com>
2019-07-17 16:28:17 +0530

    adding support to feather data structure

--- kedro/contrib/io/feather/__init__.py
+++ kedro/contrib/io/feather/__init__.py
@@ -26,16 +26,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-This module contains functionality which we might consider moving into the
-`kedro.io` module (e.g. additional ``AbstractDataSet``s and
-extensions/alternative ``DataCatalog``s.
-"""
-from .azure import CSVBlobDataSet  # NOQA
-from .bioinformatics import BioSequenceLocalDataSet  # NOQA
-from .catalog_with_default import DataCatalogWithDefault  # NOQA
-from .feather import FeatherLocalDataSet  # NOQA
-from .parquet import ParquetS3DataSet  # NOQA
-from .pyspark import SparkDataSet  # NOQA
-from .pyspark import SparkJDBCDataSet  # NOQA
-from .transformers import ProfileTimeTransformer  # NOQA
+"""kedro.contrib.io.feather provides I/O modules for feather data format.
+https://github.com/wesm/feather"""
+
+from .feather_local import FeatherLocalDataSet

30deab4935d0e9cffe49f0515983b31b3803436e
Anton Kirilenko <anton.kirilenko@wazoku.com>
2019-09-11 13:18:23 +0100

    Update the version everywhere

--- kedro/__init__.py
+++ kedro/__init__.py
@@ -31,7 +31,7 @@ data pipelines by providing uniform project templates, data abstraction,
 configuration and pipeline assembly.
 """
 
-__version__ = "0.15.0"
+__version__ = "0.15.1"
 
 
 import logging

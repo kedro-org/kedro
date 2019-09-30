96a81531948eb7531b4eeff116b31d0df2e51f7e
Kiyo <921kiyo@users.noreply.github.com>
2019-09-30 11:30:29 +0100

    Use tmp_path

--- tests/contrib/io/matplotlib/test_matplotlib_writer.py
+++ tests/contrib/io/matplotlib/test_matplotlib_writer.py
@@ -27,6 +27,7 @@
 # limitations under the License.
 
 
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pytest
@@ -34,89 +35,24 @@ import pytest
 from kedro.contrib.io.matplotlib import MatplotlibWriter
 from kedro.io import DataSetError
 
-
-def test_should_write_to_image_in_single_mode(tmpdir):
-    # generate plot
-    plt.plot(np.random.rand(1, 5)[0], np.random.rand(1, 5)[0])
-
-    # write and compare
-    trusted_filepath = str(tmpdir.join("image_we_expect.png"))
-    plt.savefig(trusted_filepath)
-
-    experimental_filepath = str(tmpdir.join("image_we_write.png"))
-    plot_writer = MatplotlibWriter(filepath=experimental_filepath)
-
-    plot_writer.save(plt)
-
-    plt.close()
-
-    with open(experimental_filepath, "rb") as f:
-        experimental_obj = f.read()
-        f.close()
-
-    with open(trusted_filepath, "rb") as f:
-        trusted_obj = f.read()
-        f.close()
-
-    assert trusted_obj == experimental_obj
+matplotlib.use("TkAgg")  # used to facilitate simple inclusion into kedro CI/CD
 
 
-def test_should_write_to_image_in_list(tmpdir):
-    plots = list()
-    # generate plots
-    for index in range(5):
-        plots.append(plt.figure())
+class TestMatplotlibWriter:
+    def test_simgle_image(self, tmp_path):
+        # generate plot
         plt.plot(np.random.rand(1, 5)[0], np.random.rand(1, 5)[0])
+
+        # write and compare
+        trusted_filepath = str(tmp_path / "image_we_expect.png")
+        plt.savefig(trusted_filepath)
+
+        experimental_filepath = str(tmp_path / "image_we_write.png")
+        plot_writer = MatplotlibWriter(filepath=experimental_filepath)
+
+        plot_writer.save(plt)
+
         plt.close()
-
-    experimental_filepath = str(tmpdir.join("list_images"))
-    plot_writer = MatplotlibWriter(filepath=experimental_filepath)
-    plot_writer.save(plots)
-
-    # write and compare
-    for index, plot in enumerate(plots):
-        string_index = str(index)
-        trusted_filepath = str(
-            tmpdir.join("image_we_expect_{}.png".format(string_index))
-        )
-        plot.savefig(trusted_filepath)
-
-        full_experimental_filepath = "{}/{}.png".format(
-            experimental_filepath, string_index
-        )
-
-        with open(full_experimental_filepath, "rb") as f:
-            full_experimental_obj = f.read()
-            f.close()
-
-        with open(trusted_filepath, "rb") as f:
-            trusted_obj = f.read()
-            f.close()
-
-        assert trusted_obj == full_experimental_obj
-
-
-def test_should_write_to_image_in_dict(tmpdir):
-    plots = dict()
-    # generate plots
-    for index in ["boo", "far"]:
-        filename = "{}.png".format(index)
-
-        plots[filename] = plt.figure()
-        plt.plot(np.random.rand(1, 5)[0], np.random.rand(1, 5)[0])
-        plt.close()
-
-    directory = str(tmpdir) + "/dict_images"
-    plot_writer = MatplotlibWriter(filepath=directory)
-    plot_writer.save(plots)
-
-    # write and compare
-    for filename, plot in plots.items():
-        experimental_filepath = str(tmpdir.join("dict_images", filename))
-        trusted_filepath = str(
-            tmpdir.join("dict_images", filename.replace(".png", "_trusted.png"))
-        )
-        plot.savefig(trusted_filepath)
 
         with open(experimental_filepath, "rb") as f:
             experimental_obj = f.read()
@@ -128,25 +64,92 @@ def test_should_write_to_image_in_dict(tmpdir):
 
         assert trusted_obj == experimental_obj
 
+    def test_list_images(self, tmp_path):
+        plots = list()
+        # generate plots
+        for index in range(5):
+            plots.append(plt.figure())
+            plt.plot(np.random.rand(1, 5)[0], np.random.rand(1, 5)[0])
+            plt.close()
 
-def test_load_should_fail(tmpdir):
-    plot_writer = MatplotlibWriter(filepath=str(tmpdir.join("some_path")))
+        experimental_filepath = str(tmp_path / "list_images")
+        plot_writer = MatplotlibWriter(filepath=experimental_filepath)
+        plot_writer.save(plots)
 
-    expected_load_error = "Loading not supported for MatplotlibWriter"
+        # write and compare
+        for index, plot in enumerate(plots):
+            string_index = str(index)
+            trusted_filepath = str(
+                tmp_path / "image_we_expect_{}.png".format(string_index)
+            )
 
-    with pytest.raises(DataSetError, match=expected_load_error):
-        plot_writer.load()
+            plot.savefig(trusted_filepath)
 
+            full_experimental_filepath = "{}/{}.png".format(
+                experimental_filepath, string_index
+            )
 
-def test_exists_functionality(tmpdir):
-    plot_object = plt.figure()
-    plt.plot(np.random.rand(1, 5)[0], np.random.rand(1, 5)[0])
-    plt.close()
+            with open(full_experimental_filepath, "rb") as f:
+                full_experimental_obj = f.read()
+                f.close()
 
-    plot_writer = MatplotlibWriter(filepath=str(tmpdir.join("some_image.png")))
+            with open(trusted_filepath, "rb") as f:
+                trusted_obj = f.read()
+                f.close()
 
-    assert not plot_writer.exists()
+            assert trusted_obj == full_experimental_obj
 
-    plot_writer.save(plot_object)
+    def test_dict_images(self, tmp_path):
+        plots = dict()
+        # generate plots
+        for index in ["boo", "far"]:
+            filename = "{}.png".format(index)
 
-    assert plot_writer.exists()
+            plots[filename] = plt.figure()
+            plt.plot(np.random.rand(1, 5)[0], np.random.rand(1, 5)[0])
+            plt.close()
+
+        directory = str(tmp_path / "dict_images")
+
+        plot_writer = MatplotlibWriter(filepath=directory)
+
+        plot_writer.save(plots)
+
+        # write and compare
+        for filename, plot in plots.items():
+            experimental_filepath = str(tmp_path / "dict_images" / filename)
+            trusted_filepath = str(
+                tmp_path / "dict_images" / filename.replace(".png", "_trusted.png")
+            )
+            plot.savefig(trusted_filepath)
+
+            with open(experimental_filepath, "rb") as f:
+                experimental_obj = f.read()
+                f.close()
+
+            with open(trusted_filepath, "rb") as f:
+                trusted_obj = f.read()
+                f.close()
+
+            assert trusted_obj == experimental_obj
+
+    def test_load_fail(self, tmp_path):
+        plot_writer = MatplotlibWriter(filepath=str(tmp_path / "some_path"))
+
+        expected_load_error = "Loading not supported for MatplotlibWriter"
+
+        with pytest.raises(DataSetError, match=expected_load_error):
+            plot_writer.load()
+
+    def test_exists(self, tmp_path):
+        plot_object = plt.figure()
+        plt.plot(np.random.rand(1, 5)[0], np.random.rand(1, 5)[0])
+        plt.close()
+
+        plot_writer = MatplotlibWriter(filepath=str(tmp_path / "some_image.png"))
+
+        assert not plot_writer.exists()
+
+        plot_writer.save(plot_object)
+
+        assert plot_writer.exists()

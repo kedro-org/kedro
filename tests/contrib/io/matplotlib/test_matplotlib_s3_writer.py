c8cc19b04c8e8954581d194b3e022bb7222e9c27
sheldontsen-qb <41573205+sheldontsen-qb@users.noreply.github.com>
2019-11-04 13:56:57 +0000

    Update tests/contrib/io/matplotlib/test_matplotlib_s3_writer.py
    Co-Authored-By: Kiyohito Kunii (Kiyo) <8097799+921kiyo@users.noreply.github.com>

--- tests/contrib/io/matplotlib/test_matplotlib_s3_writer.py
+++ tests/contrib/io/matplotlib/test_matplotlib_s3_writer.py
@@ -229,7 +229,7 @@ def test_load_fail(plot_writer):
 
 def test_exists_single(mock_single_plot, plot_writer):
     plot_writer.save(mock_single_plot)
-    plot_writer.exists()
+    assert plot_writer.exists()
 
 
 def test_exists_multiple(mock_dict_plot, plot_writer):

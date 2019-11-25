a2905f2b6fed3e477a1d6be1fe07f2665ee2eb9f
Kiyo <921kiyo@users.noreply.github.com>
2019-11-25 11:22:17 +0000

    Pass storage_options instead of credentials

--- kedro/contrib/io/dask/dask_parquet.py
+++ kedro/contrib/io/dask/dask_parquet.py
@@ -27,6 +27,7 @@
 # limitations under the License.
 
 """``ParquetDaskDataSet`` is a data set used to load and save data to parquet files using Dask"""
+from copy import deepcopy
 from typing import Any, Dict, Optional
 
 import dask.dataframe as dd
@@ -53,9 +54,10 @@ class ParquetDaskDataSet(DefaultArgumentsMixIn, AbstractDataSet):
             >>>
             >>> data_set = ParquetDaskDataSet(
             >>>                         filepath="s3://bucket_name/path/to/folder",
-            >>>                         credentials={
-            >>>                             'aws_access_key_id': 'YOUR_KEY',
-            >>>                             'aws_secret_access_key': 'YOUR SECRET'},
+            >>>                         storage_options={
+            >>>                             'client_kwargs':
+            >>>                                 'aws_access_key_id': 'YOUR_KEY',
+            >>>                                 'aws_secret_access_key': 'YOUR SECRET'},
             >>>                         save_args={"compression": "GZIP"})
             >>> data_set.save(data)
             >>> reloaded = data_set.load()
@@ -67,7 +69,7 @@ class ParquetDaskDataSet(DefaultArgumentsMixIn, AbstractDataSet):
     def __init__(
         self,
         filepath: str,
-        credentials: Optional[Dict[str, Any]] = None,
+        storage_options: Optional[Dict[str, Any]] = None,
         load_args: Optional[Dict[str, Any]] = None,
         save_args: Optional[Dict[str, Any]] = None,
     ) -> None:
@@ -77,8 +79,9 @@ class ParquetDaskDataSet(DefaultArgumentsMixIn, AbstractDataSet):
         Args:
             filepath: Path to a parquet file
                 parquet collection or the directory of a multipart parquet.
-            credentials: Credentials to any remove services, such as AWS S3 bucket
-                ``aws_access_key_id``, ``aws_secret_access_key``.
+            storage_options: Optional parameters to the backend file system driver,
+                such as credentials:
+                https://docs.dask.org/en/latest/remote-data-services.html#optional-parameters
             load_args: Additional loading options `dask.dataframe.read_parquet`:
                 https://docs.dask.org/en/latest/dataframe-api.html#dask.dataframe.read_parquet
             save_args: Additional saving options for `dask.dataframe.to_parquet`:
@@ -86,7 +89,7 @@ class ParquetDaskDataSet(DefaultArgumentsMixIn, AbstractDataSet):
         """
 
         self._filepath = filepath
-        self._credentials = credentials or {}
+        self._storage_options = deepcopy(storage_options) or {}
         super().__init__(load_args, save_args)
 
     def _describe(self) -> Dict[str, Any]:
@@ -97,10 +100,14 @@ class ParquetDaskDataSet(DefaultArgumentsMixIn, AbstractDataSet):
         )
 
     def _load(self) -> dd.DataFrame:
-        return dd.read_parquet(self._filepath, **self._load_args)
+        return dd.read_parquet(
+            self._filepath, storage_options=self._storage_options, **self._load_args
+        )
 
     def _save(self, data: dd.DataFrame) -> None:
-        data.to_parquet(self._filepath, **self._save_args)
+        data.to_parquet(
+            self._filepath, storage_options=self._storage_options, **self._save_args
+        )
 
     def _exists(self) -> bool:
         try:

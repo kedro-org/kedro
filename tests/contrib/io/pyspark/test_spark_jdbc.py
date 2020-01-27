8ff052845091ade7145fdbdf0edc156a3a319611
Tam Nguyen <Tam_Nguyen@McKinsey.com>
2020-01-27 23:20:38 +0800

    Remove redundant test

--- tests/contrib/io/pyspark/test_spark_jdbc.py
+++ tests/contrib/io/pyspark/test_spark_jdbc.py
@@ -110,12 +110,6 @@ def test_save_args(spark_jdbc_args_save_load):
 
 
 def test_except_bad_credentials(spark_jdbc_args_credentials_with_empty_password):
-    with pytest.raises(DataSetError) as excinfo:
-        mock_save(spark_jdbc_args_credentials_with_empty_password)
-    assert (
-        str(excinfo.value) == "Credential property "
-        "`password` cannot be empty. "
-        "Please provide a value."
     pattern = r"Credential property `password` cannot be empty\(.+\)"
     with pytest.raises(DataSetError, match=pattern):
         mock_save(spark_jdbc_args_credentials_with_empty_password)

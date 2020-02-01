e68120db16bb55efad63e7fd7cd438de28bf1e43
Mengzhu <xiemengzhu0526@163.com>
2020-02-01 19:30:17 +0800

    Correct data type errors in IdempotentStateStorage

--- kedro/contrib/idempotent/idempotent_state_storage.py
+++ kedro/contrib/idempotent/idempotent_state_storage.py
@@ -5,15 +5,15 @@ from typing import List
 
 class IdempotentStateStorage:
 
-    def __init__(self):
-        self.state = {
+    def __init__(self, state=None):
+        self.state = state if state else {
             # Keyed by node names, values are the current node's key
             # and the input node keys.
             # {
-            #   "key": "asdf",
+            #   "key": "asdf-asdf-sadf",
             #   "inputs": {
-            #       "node1": "qwerty",
-            #       "node2": "zxcv"
+            #       "node1": "qwer-qwer-qwer",
+            #       "node2": "zxcv-zxcv-zxcv"
             #   }
             # }
         }
@@ -30,23 +30,27 @@ class IdempotentStateStorage:
         ])
 
     def node_inputs_have_changed(self, node, inputs: List[str]):
-        expect_inputs = self.state[node]['inputs'].items()
+        expect_input_items = self.state[node]['inputs'].items()
+
+        # If any inputs are new or removed, node should be run
+        expect_inputs = [input_node for input_node, key in expect_input_items]
         if sorted(expect_inputs) != sorted(inputs):
             return True
 
         expect_input_keys = [
-            self.state[node]['inputs'].get(input_node)
-            for input_node in expect_inputs
+            key
+            for input_node, key in expect_input_items
         ]
 
         actual_input_keys = [
             self.state[input_node].get('key')
-            for input_node in expect_inputs
+            for input_node, key in expect_input_items
         ]
 
         input_keys = zip(expect_input_keys, actual_input_keys)
         input_keys_not_matching = [
             left != right
-            for left, right in input_keys
+            for left, right in set(input_keys)
         ]
+
         return any(input_keys_not_matching)

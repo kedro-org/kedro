3310515ae2ecd02916e1c5d3d90872a7bae5e15c
Zain Patel <zain.patel@quantumblack.com>
2020-03-05 12:00:18 +0000

    Remove typing

--- kedro/extras/datasets/geopandas/geojson_dataset.py
+++ kedro/extras/datasets/geopandas/geojson_dataset.py
@@ -73,7 +73,7 @@ class GeoJSONDataSet(AbstractVersionedDataSet):
     """
 
     DEFAULT_LOAD_ARGS = {}  # type: Dict[str, Any]
-    DEFAULT_SAVE_ARGS = {"driver": "GeoJSON"}  # type: Dict[str, Any]
+    DEFAULT_SAVE_ARGS = {"driver": "GeoJSON"}
 
     # pylint: disable=too-many-arguments
     def __init__(

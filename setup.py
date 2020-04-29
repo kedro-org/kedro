95d2fc76b9102e5cf3912893de7fc993ec72e613
aleks hughes <aleks.hughes@gmail.com>
2020-04-29 12:43:53 +0100

    PR feedback to allow all future SemVer TF2 versions up to and not including TF3.

--- setup.py
+++ setup.py
@@ -105,9 +105,9 @@ extras_require = {
         "tables>=3.6",
     ],
     "spark": ["pyspark>=2.2.0, <3.0", "hdfs>=2.5.8, <3.0", "s3fs>=0.3.0, <0.4.1"],
-    # only TensorFlow V2 supported for saving and loading. V1 requires HDF5 and serializes differently
     "tensorflow": [
-        "tensorflow>=2.0.0, <=2.1.0",
+        # currently only TensorFlow V2 supported for saving and loading. V1 requires HDF5 and serializes differently
+        "tensorflow>=2.0.0, <3.0",
     ]
 }
 

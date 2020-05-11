287dc17d1b64ae3eb558540657ba3e40a626633f
Deepyaman Datta <deepyaman.datta@utexas.edu>
2020-05-11 00:32:53 -0500

    Make `make legal` compute header from `LICENSE.md`

--- tools/license_and_headers.py
+++ tools/license_and_headers.py
@@ -27,46 +27,19 @@
 # limitations under the License.
 
 import glob
+import sys
+from itertools import chain
+from textwrap import indent
 
 PATHS_REQUIRING_HEADER = ["kedro", "tests"]
-LEGAL_HEADER_FILE = "legal_header.txt"
 LICENSE_MD = "LICENSE.md"
 
 RED_COLOR = "\033[0;31m"
 NO_COLOR = "\033[0m"
 
-LICENSE = """Copyright 2020 QuantumBlack Visual Analytics Limited
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
-NONINFRINGEMENT. IN NO EVENT WILL THE LICENSOR OR OTHER CONTRIBUTORS
-BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN
-ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-The QuantumBlack Visual Analytics Limited ("QuantumBlack") name and logo
-(either separately or in combination, "QuantumBlack Trademarks") are
-trademarks of QuantumBlack. The License does not grant you any right or
-license to the QuantumBlack Trademarks. You may not use the QuantumBlack
-Trademarks or any confusingly similar mark as a trademark for your product,
-or use the QuantumBlack Trademarks in any other manner that might cause
-confusion in the marketplace, including but not limited to in advertising,
-on websites, or on software.
-
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
 
 def files_at_path(path: str):
-    return glob.glob(path + "/**/*.py", recursive=True)
+    return glob.iglob(path + "/**/*.py", recursive=True)
 
 
 def files_missing_substring(file_names, substring):
@@ -90,22 +63,19 @@ def files_missing_substring(file_names, substring):
 
 
 def main():
-    exit_code = 0
-
-    with open(LEGAL_HEADER_FILE) as header_f:
-        header = header_f.read()
+    with open(LICENSE_MD) as header_f:
+        header = indent(header_f.read(), " ")
+        header = indent(header, "#", lambda line: True)
 
     # find all .py files recursively
-    files = [
-        new_file for path in PATHS_REQUIRING_HEADER for new_file in files_at_path(path)
-    ]
+    files = chain.from_iterable(files_at_path(path) for path in PATHS_REQUIRING_HEADER)
 
     # find all files which do not contain the header and are non-empty
     files_with_missing_header = list(files_missing_substring(files, header))
 
     # exit with an error and print all files without header in read, if any
     if files_with_missing_header:
-        print(
+        sys.exit(
             RED_COLOR
             + "The legal header is missing from the following files:\n- "
             + "\n- ".join(files_with_missing_header)
@@ -114,29 +84,6 @@ def main():
             + header
             + "\n"
         )
-        exit_code = 1
-
-    # check the LICENSE.md exists and has the right contents
-    try:
-        files = list(files_missing_substring([LICENSE_MD], LICENSE))
-        if files:
-            print(
-                RED_COLOR
-                + "Please make sure the LICENSE.md file "
-                + "at the root of the project "
-                + "has the right contents."
-                + NO_COLOR
-            )
-            exit(1)
-    except IOError:
-        print(
-            RED_COLOR + "Please add the LICENSE.md file at the root of the project "
-            "with the appropriate contents." + NO_COLOR
-        )
-        exit(1)
-
-    # if it doesn't exist, send a notice
-    exit(exit_code)
 
 
 if __name__ == "__main__":

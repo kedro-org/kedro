f2273aa5d8c18b299794e6b764d0cc6a5a23cc92
Andrii Ivaniuk <andrii.ivaniuk@stem.com>
2020-06-01 13:34:14 +0300

    Updated fsspec version

--- kedro/extras/datasets/tensorflow/tensorflow_model_dataset.py
+++ kedro/extras/datasets/tensorflow/tensorflow_model_dataset.py
@@ -109,6 +109,8 @@ class TensorFlowModelDataset(AbstractVersionedDataSet):
         _fs_args = copy.deepcopy(fs_args) or {}
         _credentials = copy.deepcopy(credentials) or {}
         protocol, path = get_protocol_and_path(filepath, version)
+        if protocol == "file":
+            _fs_args["auto_mkdir"] = True
 
         self._protocol = protocol
         self._fs = fsspec.filesystem(self._protocol, **_credentials, **_fs_args)

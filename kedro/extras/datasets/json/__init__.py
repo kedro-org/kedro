38446108fbc710c2c069efb716073a4bfc669be7
Tam Nguyen <Tam_Nguyen@McKinsey.com>
2020-06-25 19:24:36 +0800

    Add json dataset

---
+++ kedro/extras/datasets/json/__init__.py
@@ -0,0 +1,8 @@
+"""``AbstractDataSet`` implementation to load/save data from/to a JSON file."""
+
+__all__ = ["JSONDataSet"]
+
+from contextlib import suppress
+
+with suppress(ImportError):
+    from .json_dataset import JSONDataSet  # NOQA

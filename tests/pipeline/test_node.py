fa01439f0932d84fd30a978ef95dcc6a96878c8d
Waylon Walker <quadmx08@gmail.com>
2020-10-17 10:40:06 -0500

    try adding \

--- tests/pipeline/test_node.py
+++ tests/pipeline/test_node.py
@@ -382,7 +382,7 @@ def partial_inconsistent_input_size():
         (
             partial_inconsistent_input_size,
             (
-                r"Inputs of 'functools.partial(<function identity at .*>)' "
+                r"Inputs of 'functools.partial\(\<function identity at .*\>\)' "
                 r"expected \[\'input1\'\], but got \[\'A\', \'B\'\]"
             ),
         ),

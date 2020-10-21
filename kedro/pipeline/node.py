f42859345923a244e4cd5dac0ba66cee6bd34a2e
Waylon Walker <quadmx08@gmail.com>
2020-10-21 08:27:02 -0500

    fix add missing space

--- kedro/pipeline/node.py
+++ kedro/pipeline/node.py
@@ -547,7 +547,7 @@ class Node:  # pylint: disable=too-many-instance-attributes
                 func_name = _get_readable_func_name(func)
 
                 raise TypeError(
-                    f"Inputs of '{func_name}' function expected {list(func_args)},"
+                    f"Inputs of '{func_name}' function expected {list(func_args)}, "
                     f"but got {inputs}"
                 ) from exc
 

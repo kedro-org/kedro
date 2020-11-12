fb0ea117897de04683e9c562b6726853d3c4c73e
CHANNO <channo@oocl.com>
2020-11-12 11:26:31 +0800

    change newline='' as per pandas doc, fix empty line

--- kedro/extras/datasets/pandas/csv_dataset.py
+++ kedro/extras/datasets/pandas/csv_dataset.py
@@ -137,6 +137,7 @@ class CSVDataSet(AbstractVersionedDataSet):
 
         _fs_open_args_load.setdefault("mode", "r")
         _fs_open_args_save.setdefault("mode", "w")
+        _fs_open_args_save.setdefault("newline", "")
         self._fs_open_args_load = _fs_open_args_load
         self._fs_open_args_save = _fs_open_args_save
 

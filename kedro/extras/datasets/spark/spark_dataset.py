f8c530c489c2c71f8f820f25f0b26574336c6eca
Antony Milne <49395058+AntonyMilneQB@users.noreply.github.com>
2021-03-03 14:15:26 +0000

    Update links to spark documentation (#1007)

--- kedro/extras/datasets/spark/spark_dataset.py
+++ kedro/extras/datasets/spark/spark_dataset.py
@@ -212,14 +212,14 @@ class SparkDataSet(AbstractVersionedDataSet):
                 It is dependent on the selected file format. You can find
                 a list of read options for each supported format
                 in Spark DataFrame read documentation:
-                https://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.DataFrame
+                https://spark.apache.org/docs/latest/api/python/reference/api/pyspark.sql.DataFrame.html
             save_args: Save args passed to Spark DataFrame write options.
                 Similar to load_args this is dependent on the selected file
                 format. You can pass ``mode`` and ``partitionBy`` to specify
                 your overwrite mode and partitioning respectively. You can find
                 a list of options for each format in Spark DataFrame
                 write documentation:
-                https://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.DataFrame
+                https://spark.apache.org/docs/latest/api/python/reference/api/pyspark.sql.DataFrame.html
             version: If specified, should be an instance of
                 ``kedro.io.core.Version``. If its ``load`` attribute is
                 None, the latest version will be loaded. If its ``save``

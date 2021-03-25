dcf01d418086294749d73279d6bceea85e594f1b
Lorena Bălan <lorena.balan@quantumblack.com>
2021-03-25 16:46:32 +0000

    [KED-2502] Remove _add_src_to_path from old location (#1046)

--- kedro/framework/cli/utils.py
+++ kedro/framework/cli/utils.py
@@ -381,18 +381,3 @@ def load_entry_points(name: str) -> Sequence[click.MultiCommand]:
         except Exception as exc:
             raise KedroCliError(f"Loading {name} commands from {entry_point}") from exc
     return entry_point_commands
-
-
-def _add_src_to_path(source_dir: Path, project_path: Path) -> None:  # pragma: no cover
-    # for backwards compatibility with ipython & deployment scripts
-    # pylint: disable=import-outside-toplevel
-    from kedro.framework.startup import _add_src_to_path as real_add_src_to_path
-
-    msg = (
-        "kedro.framework.utils._add_src_to_path is deprecated. "
-        "Please import from new location kedro.framework.startup "
-        "or use `bootstrap_project()` instead for setting up "
-        "the Kedro project."
-    )
-    warnings.warn(msg, FutureWarning)
-    real_add_src_to_path(source_dir, project_path)

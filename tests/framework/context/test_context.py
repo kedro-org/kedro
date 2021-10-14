8e4011e04f92ebfabdd00bd238463a6e49de8280
Louis de Charsonville <louis_de_charsonville@mckinsey.com>
2021-10-14 18:28:49 +0200

    fix test + add more test for _update_nested_dict

--- tests/framework/context/test_context.py
+++ tests/framework/context/test_context.py
@@ -856,20 +856,20 @@ def test_validate_layers_error(layers, conflicting_datasets, mocker):
 @pytest.mark.parametrize(
     "old_dict,new_dict,expected",
     [
-        {
-            "a": 1,
-            "b": 2,
-            "c": {
-                "d": 3,
-            },
-        },
-        {"c": {"d": 5, "e": 4}},
-        {
-            "a": 1,
-            "b": 2,
-            "c": {"d": 5, "e": 4},
-        },
+        (
+            {"a": 1, "b": 2, "c": {"d": 3,},},
+            {"c": {"d": 5, "e": 4}},
+            {"a": 1, "b": 2, "c": {"d": 5, "e": 4},},
+        ),
+        ({"a": 1}, {"b": 2}, {"a": 1, "b": 2}),
+        ({"a": 1, "b": 2}, {"b": 3}, {"a": 1, "b": 3}),
+        (
+            {"a": {"a.a": 1, "a.b": 2, "a.c": {"a.c.a": 3}}},
+            {"a": {"a.c": {"a.c.b": 4}}},
+            {"a": {"a.a": 1, "a.b": 2, "a.c": {"a.c.a": 3, "a.c.b": 4}}},
+        ),
     ],
 )
 def test_update_nested_dict(old_dict: Dict, new_dict: Dict, expected: Dict):
-    assert _update_nested_dict(old_dict, new_dict) == expected
+    _update_nested_dict(old_dict, new_dict)  # _update_nested_dict change dict in place
+    assert old_dict == expected

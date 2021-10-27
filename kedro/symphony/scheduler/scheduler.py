824f69716971d0acbe8a9cf51ceb80ca423f1949
Ian Whalen <46673650+ianwhalenqb@users.noreply.github.com>
2021-10-27 15:19:02 +0100

    Add scheduler base and ToposortScheduler (#1)

--- kedro/symphony/scheduler/scheduler.py
+++ kedro/symphony/scheduler/scheduler.py
@@ -1,2 +1,43 @@
 """``AbstractScheduler`` class definition.
 """
+
+import abc
+from typing import List
+
+from kedro.pipeline import Pipeline
+from kedro.pipeline.node import Node
+
+
+class AbstractScheduler(abc.ABC):
+    """Base scheduler class.
+
+    Schedulers are just iterators that return a list of nodes to execute at
+    each iteration.
+    """
+
+    def __init__(self, pipeline: Pipeline):
+        """Constructor.
+
+        Args:
+            pipeline: Pipeline object to schedule the execution of.
+        """
+        self.pipeline = pipeline
+
+    def __iter__(self):
+        """Iter dunder method.
+
+        Returns:
+            self.
+        """
+        return self
+
+    @abc.abstractmethod
+    def __next__(self) -> List[Node]:
+        """List of nodes that are next to be executed.
+
+        Raises:
+            StopIteration when we're done iterating.
+
+        Returns:
+            List of nodes.
+        """
---
+++ kedro/symphony/scheduler/toposort_scheduler.py
@@ -0,0 +1,47 @@
+"""Topologically sorted scheduler definition.
+"""
+
+from typing import List
+
+from kedro.pipeline import Pipeline
+from kedro.pipeline.node import Node
+from kedro.symphony.scheduler.scheduler import AbstractScheduler
+
+
+class ToposortScheduler(AbstractScheduler):
+    """The most basic schedule.
+
+    Returns the nodes that are next in the topologically sorted order.
+    """
+    def __init__(self, pipeline: Pipeline):
+        """Constructor.
+
+        Args:
+            pipeline: Pipeline object to schedule the execution of.
+        """
+        super().__init__(pipeline)
+
+        self.node_dependencies = pipeline.node_dependencies
+        self.todo_nodes = set(self.node_dependencies.keys())
+        self.done_nodes = set()
+
+    def __next__(self) -> List[Node]:
+        """Get the next list of nodes in topological order.
+
+        Raises:
+            StopIteration when we're done iterating.
+
+        Returns:
+            List of Nodes.
+        """
+        if not self.todo_nodes:
+            raise StopIteration
+
+        ready = {
+            n for n in self.todo_nodes if self.node_dependencies[n] <= self.done_nodes
+        }
+
+        self.todo_nodes = self.todo_nodes.difference(ready)
+        self.done_nodes = self.done_nodes.union(ready)
+
+        return list(ready)

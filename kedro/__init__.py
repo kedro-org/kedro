b735975051a985540ba3711dc4214f369fdf8265
Deepyaman Datta <deepyaman.datta@utexas.edu>
2021-10-27 17:50:02 +0100

    Make version PEP 440-compliant

--- kedro/__init__.py
+++ kedro/__init__.py
@@ -31,7 +31,7 @@ data pipelines by providing uniform project templates, data abstraction,
 configuration and pipeline assembly.
 """
 
-__version__ = "0.17.5.symphony"
+__version__ = "0.17.5+symphony"
 
 
 import logging

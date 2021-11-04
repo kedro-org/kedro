6184a4a1346e62b0767297b675fafc77643a6513
Lorena Bălan <lorena.balan@quantumblack.com>
2021-11-04 11:03:50 +0000

    [KED-2926] Deprecate decorators (#1017)
    Signed-off-by: Laurens Vijnck <laurens_vijnck@mckinsey.com>

--- kedro/extras/decorators/__init__.py
+++ kedro/extras/decorators/__init__.py
@@ -1,1 +1,10 @@
 """``kedro.extras.decorators`` provides Node/Pipeline Decorators."""
+import warnings
+
+warnings.simplefilter("default", DeprecationWarning)
+
+warnings.warn(
+    "Support for decorators will be deprecated in Kedro 0.18.0. "
+    "Please use Hooks to extend the behaviour of a node or pipeline.",
+    DeprecationWarning,
+)

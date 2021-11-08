b030fb64b3a0017ce358fe885fa3ab4b85db100f
lorenabalan <lorena.balan@quantumblack.com>
2021-11-08 15:08:45 +0000

    Deprecate transformers

--- kedro/extras/transformers/__init__.py
+++ kedro/extras/transformers/__init__.py
@@ -1,6 +1,15 @@
 """``kedro.extras.transformers`` is the home of Kedro's dataset transformers."""
+import warnings
 
 from .memory_profiler import ProfileMemoryTransformer
 from .time_profiler import ProfileTimeTransformer
 
 __all__ = ["ProfileMemoryTransformer", "ProfileTimeTransformer"]
+
+warnings.simplefilter("default", DeprecationWarning)
+
+warnings.warn(
+    "Support for transformers will be deprecated in Kedro 0.18.0. "
+    "Please use Hooks `before_dataset_loaded` or `after_dataset_loaded` instead.",
+    DeprecationWarning,
+)

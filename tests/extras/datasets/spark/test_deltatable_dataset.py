c6efb74b529532a31668ca5d75c051bf3b00300b
lorenabalan <lorena.balan@quantumblack.com>
2021-11-17 11:32:50 +0000

    Write minimal unit test

--- tests/extras/datasets/spark/test_deltatable_dataset.py
+++ tests/extras/datasets/spark/test_deltatable_dataset.py
@@ -1,15 +1,41 @@
 import pytest
+from delta import DeltaTable
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 
-from kedro.extras.datasets.spark import DeltaTableDataset
+from kedro.extras.datasets.spark import DeltaTableDataset, SparkDataSet
 
 
 @pytest.fixture(autouse=True)
-def spark_session_autouse(spark_session):
-    # all the tests in this file require Spark
+def delta_spark_session(spark_session):
     return spark_session
 
 
+@pytest.fixture
+def sample_spark_df():
+    schema = StructType(
+        [
+            StructField("name", StringType(), True),
+            StructField("age", IntegerType(), True),
+        ]
+    )
+
+    data = [("Alex", 31), ("Bob", 12), ("Clarke", 65), ("Dave", 29)]
+
+    return SparkSession.builder.getOrCreate().createDataFrame(data, schema)
+
+
 class TestDeltaTableDataSet:
-    def test_load(self, tmp_path):
-        temp_path = (tmp_path / "data").as_posix()
-        DeltaTableDataset(filepath=temp_path)
+    def test_load(self, tmp_path, sample_spark_df):
+        filepath = (tmp_path / "data.csv").as_posix()
+        spark_ds = SparkDataSet(filepath=filepath, file_format="csv")
+        spark_ds.save(sample_spark_df)
+
+        spark_delta_ds = SparkDataSet(
+            filepath=filepath, file_format="delta", save_args={"mode": "overwrite"}
+        )
+        spark_delta_ds.save(sample_spark_df)
+        delta_ds = DeltaTableDataset(filepath=filepath)
+        delta_table = delta_ds.load()
+
+        assert isinstance(delta_table, DeltaTable)

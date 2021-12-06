6486faeb709df4c1ab916ade9d3db2fcd7352285
Antony Milne <49395058+AntonyMilneQB@users.noreply.github.com>
2021-12-06 12:45:32 +0000

    Update deltatable_dataset.py

--- kedro/extras/datasets/spark/deltatable_dataset.py
+++ kedro/extras/datasets/spark/deltatable_dataset.py
@@ -1,4 +1,4 @@
-"""``AbstractVersionedDataSet`` implementation to access DeltaTables using
+"""``AbstractDataSet`` implementation to access DeltaTables using
 ``delta-spark``
 """
 from copy import deepcopy

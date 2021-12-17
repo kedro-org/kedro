58186d739b5940b06eb60f10491391dde8112ad5
Antony Milne <49395058+AntonyMilneQB@users.noreply.github.com>
2021-12-17 17:02:59 +0000

    [KED-2961] Move scripts from private-kedro-sync (#1107)
    * Pre-tidy
    
    * Simplify linux e2e_tests
    
    * Simplify linux e2e_tests
    
    * Move e2e_tests from command to job
    
    * String python version
    
    * Comment out unwanted tests for now
    
    * Try parametrised command name
    
    * Try conditionals
    
    * Indent
    
    * Indent
    
    * Urgh yaml
    
    * Pass arguments
    
    * Try win_e2e_tests separately
    
    * Try win_e2e_tests separately
    
    * Oops
    
    * Fix windows hopefully
    
    * URgh yaml
    
    * Alter lint to matrix
    
    * De-parametrise executor
    
    * Rename executors
    
    * Oops
    
    * Add win_pip_compile
    
    * Add win unit tests
    
    * Add viz build
    
    * Cosmetic changes
    
    * Refactor into setup and win_setup
    
    * Refactor into setup and win_setup
    
    * Refactor into setup and win_setup
    
    * Refactor into setup and win_setup
    
    * Back to parametrised executor
    
    * Back to parametrised executor
    
    * Add unit_tests
    
    * Clarify comment
    
    * Add all files to tools/sync
    
    * Remove unwanted workflows
    
    * Alter file paths and remove clone_repo
    
    * Replace build_kedro with one job
    
    * Replace build_kedro with one job
    
    * Trigger sync on merge to main
    
    * Trigger on merge to main rather than nightly
    
    * Remove old private-kedro ssh
    
    * Merge into main config.yml file
    
    * Add unless
    
    * Correct to match schema
    
    * Lint
    
    * Tidy
    
    * Hopefully correct a filepath?
    
    * Alter PR message
    
    * Update tools/circleci/github_scripts/merge.sh
    
    Co-authored-by: Lorena Bălan <lorena.balan@quantumblack.com>
    
    * Update requirements
    
    * Remove SSH and pre-commit install
    
    * Post-merge fix
    
    * Update text
    
    * Make two different CIRCLE_TOKENs
    
    * Update .circleci/config.yml
    
    * Move automatic merging to hourly rather than main_updated
    
    * Move merge to above trigger release workflow
    
    * Add git checkout after merge.sh
    
    * Add git checkout after merge.sh
    
    * New line!
    
    Co-authored-by: Lorena Bălan <lorena.balan@quantumblack.com>
    Signed-off-by: Laurens Vijnck <laurens_vijnck@mckinsey.com>

---
+++ tools/circleci/github_scripts/attempt_merge_pr.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+# Script to attempt to merge an automatic PR from main to develop.
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+SOURCE_BRANCH=$1
+TARGET_BRANCH=$2
+GITHUB_TAGGING_TOKEN=$3
+GITHUB_USER="quantumblacklabs"
+GITHUB_REPO="kedro"
+
+# Array of GitHub mergeable states that are valid to proceed with automatic PR merging.
+# Adding "unstable" can be convenient, as it allows to auto-merge pull requests that
+# pass just the required CI checks (whereas "clean" requires all CI checks to pass,
+# regardless of whether they are required or not). More info:
+# https://docs.github.com/en/graphql/reference/enums#mergestatestatus
+# https://github.com/octokit/octokit.net/issues/1763
+VALID_MERGEABLE_STATES=("clean" "unstable")
+
+find_github_pr() {
+  # Find a PR from source to target branch
+  # Returns PR number if GitHub returned exactly one such PR
+  endpoint="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/pulls?base=${TARGET_BRANCH}&head=${GITHUB_USER}:${SOURCE_BRANCH}&state=open"
+  response=$(curl --silent --header "Authorization: token ${GITHUB_TAGGING_TOKEN}" "${endpoint}")
+  num_open_prs=$(echo "$response" | tr '\r\n' ' ' | jq "length")
+  if [ "$num_open_prs" -eq 1 ]; then
+    echo "$response" | tr '\r\n' ' ' | jq ".[0].number"
+  fi
+}
+
+check_pr_mergeable() {
+  # Check that the given PR is in a mergeable state
+  pr=$1
+  endpoint="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/pulls/${pr}"
+  response=$(curl --silent --header "Authorization: token ${GITHUB_TAGGING_TOKEN}" "${endpoint}")
+  mergeable=$(echo "${response}" | tr '\r\n' ' ' | jq ".mergeable // false")  # default to false
+  echo "PR ${pr} mergeable: ${mergeable}"
+  mergeable_state=$(echo "${response}" | tr '\r\n' ' ' | jq --raw-output ".mergeable_state // \"unknown\"")
+  echo "PR ${pr} mergeable_state: ${mergeable_state}"
+  [ "${mergeable}" == true ] && [[ " ${VALID_MERGEABLE_STATES[@]} " =~ " ${mergeable_state} " ]]
+}
+
+toggle_merge_commits() {
+  # Turns merge commits on or off for the repository
+  allow_merge_commit=$1
+  endpoint="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}"
+  payload="{\"allow_merge_commit\": ${allow_merge_commit}}"
+  status_code=$(curl -X PATCH \
+    --silent \
+    --header "Authorization: token ${GITHUB_TAGGING_TOKEN}" \
+    --header "Content-Type: application/json" \
+    --data "${payload}" \
+    --output /dev/null \
+    --write-out "%{http_code}\n" \
+    "${endpoint}")
+  [ "${status_code}" -eq 200 ]
+}
+
+delete_git_ref() {
+  # Delete a reference
+  git_ref=$1
+  endpoint="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/git/refs/${git_ref}"
+  status_code=$(curl -X DELETE \
+    --silent \
+    --header "Authorization: token ${GITHUB_TAGGING_TOKEN}" \
+    --output /dev/null \
+    --write-out "%{http_code}\n" \
+    "${endpoint}")
+  [ "${status_code}" -eq 204 ]
+}
+
+merge_pr() {
+  # Merge a given PR using merge commit
+  pr=$1
+  toggle_merge_commits true
+  endpoint="https://api.github.com/repos/${GITHUB_USER}/${GITHUB_REPO}/pulls/${pr}/merge"
+  payload='{"merge_method": "merge"}'
+  response=$(curl -X PUT \
+    --silent \
+    --header "Authorization: token ${GITHUB_TAGGING_TOKEN}" \
+    --header "Content-Type: application/json" \
+    --data "${payload}" \
+    "${endpoint}")
+  toggle_merge_commits false
+  merged=$(echo "${response}" | tr '\r\n' ' ' | jq ".merged // false")  # default to false
+  if [ "${merged}" == true ]; then
+    echo "PR ${pr} successfully merged"
+    delete_git_ref "heads/${SOURCE_BRANCH}"
+    echo "Branch ${SOURCE_BRANCH} successfully deleted"
+  else
+    message=$(echo "${response}" | tr '\r\n' ' ' | jq --raw-output ".message")
+    echo "PR ${pr} NOT merged. Message: ${message}"
+  fi
+  [ "${merged}" == true ]
+}
+
+pr_number=$(find_github_pr)
+
+if [ -z "${pr_number}" ]; then
+  echo "No PR found from ${SOURCE_BRANCH} to ${TARGET_BRANCH}"
+  exit 0
+fi
+
+if check_pr_mergeable "${pr_number}"; then
+  merge_pr "${pr_number}"
+else
+  echo "PR ${pr_number} is not in a mergeable state"
+fi

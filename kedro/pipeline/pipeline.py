a6b704e0cfa5b763f59502136fd7bc961fa2f7cf
Antony Milne <antony.milne@quantumblack.com>
2022-01-06 17:28:32 +0000

    Change Pipeline.__init__

--- kedro/pipeline/pipeline.py
+++ kedro/pipeline/pipeline.py
@@ -132,6 +132,13 @@ class Pipeline:  # pylint: disable=too-many-public-methods
             >>>
 
         """
+        self._set_pipeline(nodes, tags)
+        if inputs or outputs or parameters or namespace:
+            new_nodes = self._transform_pipeline(inputs, outputs, parameters, namespace)
+            self._set_pipeline(new_nodes, tags)
+            # TODO: don't really need to carry around tags here?
+
+    def _set_pipeline(self, nodes, tags):
         if nodes is None:
             raise ValueError(
                 "`nodes` argument of `Pipeline` is None. It must be an "
@@ -161,13 +168,126 @@ class Pipeline:  # pylint: disable=too-many-public-methods
                 self._nodes_by_input[_strip_transcoding(input_)].add(node)
 
         # output -> node with output
-        self._nodes_by_output = {}  # type: Dict[str, # Node]
+        self._nodes_by_output = {}  # type: Dict[str, Node]
         for node in nodes:
             for output in node.outputs:
                 self._nodes_by_output[_strip_transcoding(output)] = node
 
         self._nodes = nodes
         self._topo_sorted_nodes = _topologically_sorted(self.node_dependencies)
+
+    def _transform_pipeline(
+        self,
+        inputs: Union[str, Set[str], Dict[str, str]],
+        outputs: Union[str, Set[str], Dict[str, str]],
+        parameters: Dict[str, str],
+        namespace: str,
+    ):
+        """Create a copy of the pipeline and its nodes,
+        with some dataset names and node names modified.
+
+        Args:
+            inputs: A name or collection of input names to be exposed as connection points
+                to other pipelines upstream.
+                When str or Set[str] is provided, the listed input names will stay
+                the same as they are named in the provided pipeline.
+                When Dict[str, str] is provided, current input names will be
+                mapped to new names.
+                Must only refer to the pipeline's free inputs.
+            outputs: A name or collection of names to be exposed as connection points
+                to other pipelines downstream.
+                When str or Set[str] is provided, the listed output names will stay
+                the same as they are named in the provided pipeline.
+                When Dict[str, str] is provided, current output names will be
+                mapped to new names.
+                Can refer to both the pipeline's free outputs, as well as
+                intermediate results that need to be exposed.
+            parameters: A map of existing parameter to the new one.
+            namespace: A prefix to give to all dataset names,
+                except those explicitly named with the `inputs`/`outputs`
+                arguments, and parameter references (`params:` and `parameters`).
+
+        Raises:
+            ModularPipelineError: When inputs, outputs or parameters are incorrectly
+                specified, or they do not exist on the original pipeline.
+            ValueError: When underlying pipeline nodes inputs/outputs are not
+                any of the expected types (str, dict, list, or None).
+
+        Returns:
+            A new ``Pipeline`` object with the new nodes, modified as requested.
+        """
+        # pylint: disable=protected-access
+        inputs = _to_dict(inputs)
+        outputs = _to_dict(outputs)
+        parameters = _to_dict(parameters)
+
+        _validate_datasets_exist(inputs.keys(), outputs.keys(), parameters.keys(), self)
+        _validate_inputs_outputs(inputs.keys(), outputs.keys(), self)
+
+        mapping = {**inputs, **outputs, **parameters}
+
+        def _prefix(name: str) -> str:
+            return f"{namespace}.{name}" if namespace else name
+
+        def _is_transcode_base_in_mapping(name: str) -> bool:
+            base_name, _ = _transcode_split(name)
+            return base_name in mapping
+
+        def _map_transcode_base(name: str):
+            base_name, transcode_suffix = _transcode_split(name)
+            return TRANSCODING_SEPARATOR.join((mapping[base_name], transcode_suffix))
+
+        def _rename(name: str):
+            rules = [
+                # if name mapped to new name, update with new name
+                (lambda n: n in mapping, lambda n: mapping[n]),
+                # if it's a parameter, leave as is (don't namespace)
+                (_is_parameter, lambda n: n),
+                # if transcode base is mapped to a new name, update with new base
+                (_is_transcode_base_in_mapping, _map_transcode_base),
+                # if namespace given, prefix name using that namespace
+                (lambda n: bool(namespace), _prefix),
+            ]
+
+            for predicate, processor in rules:
+                if predicate(name):
+                    return processor(name)
+
+            # leave name as is
+            return name
+
+        def _process_dataset_names(
+            datasets: Union[None, str, List[str], Dict[str, str]]
+        ) -> Union[None, str, List[str], Dict[str, str]]:
+            if datasets is None:
+                return None
+            if isinstance(datasets, str):
+                return _rename(datasets)
+            if isinstance(datasets, list):
+                return [_rename(name) for name in datasets]
+            if isinstance(datasets, dict):
+                return {key: _rename(value) for key, value in datasets.items()}
+
+            raise ValueError(  # pragma: no cover
+                f"Unexpected input {datasets} of type {type(datasets)}"
+            )
+
+        def _copy_node(node: Node) -> Node:
+            new_namespace = node.namespace
+            if namespace:
+                new_namespace = (
+                    f"{namespace}.{node.namespace}" if node.namespace else namespace
+                )
+
+            return node._copy(
+                inputs=_process_dataset_names(node._inputs),
+                outputs=_process_dataset_names(node._outputs),
+                namespace=new_namespace,
+            )
+
+        # TODO: maybe change copy logic to just update?
+        new_nodes = [_copy_node(n) for n in self.nodes]
+        return new_nodes
 
     def __repr__(self):  # pragma: no cover
         """Pipeline ([node1, ..., node10 ...], name='pipeline_name')"""
@@ -901,122 +1021,6 @@ def _validate_datasets_exist(
             f"Failed to map datasets and/or parameters: "
             f"{', '.join(sorted(non_existent))}"
         )
-
-
-def _transform_pipeline(
-    pipe: Pipeline,
-    *,
-    inputs: Union[str, Set[str], Dict[str, str]] = None,
-    outputs: Union[str, Set[str], Dict[str, str]] = None,
-    parameters: Dict[str, str] = None,
-    namespace: str = None,
-) -> Pipeline:
-    """Create a copy of the pipeline and its nodes,
-    with some dataset names and node names modified.
-
-    Args:
-        pipe: Original modular pipeline to integrate
-        inputs: A name or collection of input names to be exposed as connection points
-            to other pipelines upstream.
-            When str or Set[str] is provided, the listed input names will stay
-            the same as they are named in the provided pipeline.
-            When Dict[str, str] is provided, current input names will be
-            mapped to new names.
-            Must only refer to the pipeline's free inputs.
-        outputs: A name or collection of names to be exposed as connection points
-            to other pipelines downstream.
-            When str or Set[str] is provided, the listed output names will stay
-            the same as they are named in the provided pipeline.
-            When Dict[str, str] is provided, current output names will be
-            mapped to new names.
-            Can refer to both the pipeline's free outputs, as well as
-            intermediate results that need to be exposed.
-        parameters: A map of existing parameter to the new one.
-        namespace: A prefix to give to all dataset names,
-            except those explicitly named with the `inputs`/`outputs`
-            arguments, and parameter references (`params:` and `parameters`).
-
-    Raises:
-        ModularPipelineError: When inputs, outputs or parameters are incorrectly
-            specified, or they do not exist on the original pipeline.
-        ValueError: When underlying pipeline nodes inputs/outputs are not
-            any of the expected types (str, dict, list, or None).
-
-    Returns:
-        A new ``Pipeline`` object with the new nodes, modified as requested.
-    """
-    # pylint: disable=protected-access
-    inputs = _to_dict(inputs)
-    outputs = _to_dict(outputs)
-    parameters = _to_dict(parameters)
-
-    _validate_datasets_exist(inputs.keys(), outputs.keys(), parameters.keys(), pipe)
-    _validate_inputs_outputs(inputs.keys(), outputs.keys(), pipe)
-
-    mapping = {**inputs, **outputs, **parameters}
-
-    def _prefix(name: str) -> str:
-        return f"{namespace}.{name}" if namespace else name
-
-    def _is_transcode_base_in_mapping(name: str) -> bool:
-        base_name, _ = _transcode_split(name)
-        return base_name in mapping
-
-    def _map_transcode_base(name: str):
-        base_name, transcode_suffix = _transcode_split(name)
-        return TRANSCODING_SEPARATOR.join((mapping[base_name], transcode_suffix))
-
-    def _rename(name: str):
-        rules = [
-            # if name mapped to new name, update with new name
-            (lambda n: n in mapping, lambda n: mapping[n]),
-            # if it's a parameter, leave as is (don't namespace)
-            (_is_parameter, lambda n: n),
-            # if transcode base is mapped to a new name, update with new base
-            (_is_transcode_base_in_mapping, _map_transcode_base),
-            # if namespace given, prefix name using that namespace
-            (lambda n: bool(namespace), _prefix),
-        ]
-
-        for predicate, processor in rules:
-            if predicate(name):
-                return processor(name)
-
-        # leave name as is
-        return name
-
-    def _process_dataset_names(
-        datasets: Union[None, str, List[str], Dict[str, str]]
-    ) -> Union[None, str, List[str], Dict[str, str]]:
-        if datasets is None:
-            return None
-        if isinstance(datasets, str):
-            return _rename(datasets)
-        if isinstance(datasets, list):
-            return [_rename(name) for name in datasets]
-        if isinstance(datasets, dict):
-            return {key: _rename(value) for key, value in datasets.items()}
-
-        raise ValueError(  # pragma: no cover
-            f"Unexpected input {datasets} of type {type(datasets)}"
-        )
-
-    def _copy_node(node: Node) -> Node:
-        new_namespace = node.namespace
-        if namespace:
-            new_namespace = (
-                f"{namespace}.{node.namespace}" if node.namespace else namespace
-            )
-
-        return node._copy(
-            inputs=_process_dataset_names(node._inputs),
-            outputs=_process_dataset_names(node._outputs),
-            namespace=new_namespace,
-        )
-
-    new_nodes = [_copy_node(n) for n in pipe.nodes]
-
-    return Pipeline(new_nodes)
 
 
 def _to_dict(element: Union[None, str, Set[str], Dict[str, str]]) -> Dict[str, str]:

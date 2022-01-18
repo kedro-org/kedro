230763a3ef01ae23b6955b491433dcff82643548
Antony Milne <49395058+AntonyMilneQB@users.noreply.github.com>
2022-01-18 14:37:09 +0000

    [KED-2419] Make pipeline and Pipeline consistent, take 2 (#1147)

--- kedro/templates/pipeline/{{ cookiecutter.pipeline_name }}/pipeline.py
+++ kedro/templates/pipeline/{{ cookiecutter.pipeline_name }}/pipeline.py
@@ -3,8 +3,8 @@ This is a boilerplate pipeline '{{ cookiecutter.pipeline_name }}'
 generated using Kedro {{ cookiecutter.kedro_version }}
 """
 
-from kedro.pipeline import Pipeline, node
+from kedro.pipeline import Pipeline, node, pipeline
 
 
-def create_pipeline(**kwargs):
-    return Pipeline([])
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])

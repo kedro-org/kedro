11c259162101c044d1ae02db1c6a1e05008ac416
juan-carlos-calvo <juancarlos.calvo@quantumblack.com>
2022-01-21 13:13:16 -0300

    added to jsonschema, added multiple pickle backend support, and reference to docs
    Signed-off-by: juan-carlos-calvo <juancarlos.calvo@quantumblack.com>
    Signed-off-by: jccalvojackson <juancarlos.calvojackson@gmail.com>

--- kedro/extras/datasets/redis/redis_dataset.py
+++ kedro/extras/datasets/redis/redis_dataset.py
@@ -2,6 +2,7 @@
 functionality is supported by the redis library, so it supports all allowed
 options for instantiating the redis app ``from_url`` and setting a value."""
 
+import importlib
 import os
 import pickle
 from copy import deepcopy
@@ -53,35 +54,101 @@ class RedisDataSet(AbstractDataSet):
         >>> assert data.equals(reloaded)
     """
 
-    default_redis_url = os.getenv("REDIS_URL", "redis://127.0.0.1:6379")
-    DEFAULT_FROM_URL_ARGS = {"url": default_redis_url}  # type: Dict[str, Any]
+    DEFAULT_REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379")
+    DEFAULT_LOAD_ARGS = {}  # type: Dict[str, Any]
     DEFAULT_SAVE_ARGS = {}  # type: Dict[str, Any]
 
     def __init__(
         self,
         key: str,
-        from_url_args: Dict[str, Any] = None,
+        backend: str = "pickle",
+        load_args: Dict[str, Any] = None,
         save_args: Dict[str, Any] = None,
+        credentials: Dict[str, Any] = None,
+        redis_args: Dict[str, Any] = None,
     ) -> None:
-        """Construct instance of `RedisDataSet`.
+        """Creates a new instance of `RedisDataSet`. This loads/saves data from/to
+        a Redis database while deserializing/serializing. supports custom backends to
+        serialize/deserialize objects.
+
+        Example backends that are compatible (non-exhaustive):
+            * `pickle`
+            * `dill`
+            * `compress_pickle`
+
+        Example backends that are incompatible:
+            * `torch`
 
         Args:
-            key: The key to use for saving/ loading object to Redis redis_url:
-            Redis database url. Otherwise it is extracted from environment
-            variables.
-            from_url_args: Additional arguments to pass to `redis.Redis.from_url`.
-            save_args: Additional arguments to pass to `redis.Redis.set`.
+            key: The key to use for saving/loading object to Redis.
+            backend: Backend to use, must be an import path to a module which satisfies the
+                ``pickle`` interface. That is, contains a `loads` and `dumps` function.
+                Defaults to 'pickle'.
+            load_args: Pickle options for loading pickle files.
+                You can pass in arguments that the backend load function specified accepts, e.g:
+                pickle.loads: https://docs.python.org/3/library/pickle.html#pickle.loads
+                dill.loads: https://dill.readthedocs.io/en/latest/dill.html#dill._dill.loads
+                compress_pickle.loads:
+                https://lucianopaz.github.io/compress_pickle/html/api/compress_pickle.html#compress_pickle.compress_pickle.loads
+                All defaults are preserved.
+            save_args: Pickle options for saving pickle files.
+                You can pass in arguments that the backend dump function specified accepts, e.g:
+                pickle.dumps: https://docs.python.org/3/library/pickle.html#pickle.dump
+                dill.dumps: https://dill.readthedocs.io/en/latest/dill.html#dill._dill.dumps
+                compress_pickle.dumps:
+                https://lucianopaz.github.io/compress_pickle/html/api/compress_pickle.html#compress_pickle.compress_pickle.dumps
+                All defaults are preserved.
+            credentials: Credentials required to get access to the redis server.
+                E.g. `{"password": None}`.
+            redis_args: Extra arguments to pass into the redis client constructor ``redis.StrictRedis.from_url``.
+                (e.g. `{"socket_timeout": 10}`), as well as to pass to the ``redis.StrictRedis.set``
+                through nested keys `from_url_args` and `set_args`. Here you can find all available
+                arguments for `from_url`:
+                https://redis-py.readthedocs.io/en/stable/connections.html?highlight=from_url#redis.Redis.from_url
+                All defaults are preserved, except `url`, which is set to `redis://127.0.0.1:6379`.
+                You could also specify the url through the env variable ``REDIS_URL``.
 
+        Raises:
+            ValueError: If ``backend`` does not satisfy the `pickle` interface.
+            ImportError: If the ``backend`` module could not be imported.
         """
         super().__init__()
+        try:
+            imported_backend = importlib.import_module(backend)
+        except ImportError as exc:
+            raise ImportError(
+                f"Selected backend '{backend}' could not be imported. "
+                "Make sure it is installed and importable."
+            ) from exc
+
+        if not (
+            hasattr(imported_backend, "loads") and hasattr(imported_backend, "dumps")
+        ):
+            raise ValueError(
+                f"Selected backend '{backend}' should satisfy the pickle interface. "
+                "Missing one of `loads` and `dumps` on the backend."
+            )
+
+        self._backend = imported_backend
+
         self._key = key
-        self._from_url_args = deepcopy(self.DEFAULT_FROM_URL_ARGS)
-        if from_url_args is not None:
-            self._from_url_args.update(from_url_args)
+
+        _redis_args = deepcopy(redis_args) or {}
+        self._redis_from_url_args = _redis_args.pop("from_url_args", {})
+        self._redis_from_url_args.setdefault("url", self.DEFAULT_REDIS_URL)
+        self._redis_set_args = _redis_args.pop("set_args", {})
+        _credentials = deepcopy(credentials) or {}
+
+        self._load_args = deepcopy(self.DEFAULT_LOAD_ARGS)
+        if load_args is not None:
+            self._load_args.update(load_args)
         self._save_args = deepcopy(self.DEFAULT_SAVE_ARGS)
         if save_args is not None:
             self._save_args.update(save_args)
-        self._redis_db = redis.Redis.from_url(**self._from_url_args)
+
+        self._redis_db = redis.Redis.from_url(
+            **self._redis_from_url_args, **_credentials
+        )
 
     def _describe(self) -> Dict[str, Any]:
         return dict(key=self._key, **self._from_url_args)
@@ -89,11 +156,15 @@ class RedisDataSet(AbstractDataSet):
     # `redis_db` mypy does not work since it is optional and optional is not
     # accepted by pickle.loads.
     def _load(self) -> Any:
-        return pickle.loads(self._redis_db.get(self._key))  # type: ignore
+        return self._backend.loads(self._redis_db.get(self._key), **self._load_args)  # type: ignore
 
     def _save(self, data: Any) -> None:
         try:
-            self._redis_db.set(self._key, pickle.dumps(data), **self._save_args)
+            self._redis_db.set(
+                self._key,
+                self._backend.dumps(data, **self._save_args),
+                **self._redis_set_args,
+            )
         except Exception as exc:
             raise DataSetError(
                 f"{data.__class__} was not serialized due to: {exc}"

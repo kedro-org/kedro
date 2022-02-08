48e5d8243241f20a575e17cbd20c0b10e81e79c1
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2022-02-08 12:43:23 +0000

    test

--- tests/framework/cli/test_cli.py
+++ tests/framework/cli/test_cli.py
@@ -15,6 +15,7 @@ from kedro.framework.cli.catalog import catalog_cli
 from kedro.framework.cli.cli import KedroCLI, _init_plugins, cli
 from kedro.framework.cli.jupyter import jupyter_cli
 from kedro.framework.cli.pipeline import pipeline_cli
+from kedro.framework.cli.micropkg import micropkg_cli
 from kedro.framework.cli.project import project_group
 from kedro.framework.cli.registry import registry_cli
 from kedro.framework.cli.starters import create_cli
@@ -385,6 +386,7 @@ class TestKedroCLI:
             catalog_cli,
             jupyter_cli,
             pipeline_cli,
+            micropkg_cli,
             project_group,
             registry_cli,
         ]

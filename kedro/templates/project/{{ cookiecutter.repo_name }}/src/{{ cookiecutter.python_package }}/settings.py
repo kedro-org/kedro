0b6afb1145a84459c357c1072227b491f977a657
Antony Milne <antony.milne@quantumblack.com>
2022-02-14 16:46:33 +0000

    Give useful docstring
    Signed-off-by: Antony Milne <antony.milne@quantumblack.com>

--- kedro/templates/project/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
+++ kedro/templates/project/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
@@ -1,6 +1,6 @@
-"""Project settings."""
-
-# TODO: LINK TO MORE DOCS and default values
+"""Project settings. There is no need to edit this file unless you want to change values
+from the Kedro defaults. For further information, including these default values, see
+https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 
 #############
 # LIFECYCLE #
@@ -13,9 +13,10 @@
 # Installed plugins for which to disable hook auto-registration.
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
 
-# Class and constructor keyword arguments that define how and where to store KedroSession data.
+# Class that manages storing KedroSession data.
 # from kedro.framework.session.store import ShelveStore
 # SESSION_STORE_CLASS = ShelveStore
+# Keyword arguments to pass to the `SESSION_STORE_CLASS` constructor.
 # SESSION_STORE_ARGS = {
 #     "path": "./sessions"
 # }
@@ -30,9 +31,10 @@
 # Directory that holds configuration.
 # CONF_SOURCE = "conf"
 
-# Class and constructor keyword argument that define how configuration is loaded.
+# Class that manages how configuration is loaded.
 # from kedro.config import TemplatedConfigLoader
 # CONFIG_LOADER_CLASS = TemplatedConfigLoader
+# Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 # CONFIG_LOADER_ARGS = {
 #     "globals_pattern": "*globals.yml",
 # }

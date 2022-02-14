8d77d3ff575f84778f07300bf6ac2bcf51e6a08c
Antony Milne <antony.milne@quantumblack.com>
2022-02-14 15:56:34 +0000

    Reword descriptions
    Signed-off-by: Antony Milne <antony.milne@quantumblack.com>

--- kedro/templates/project/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
+++ kedro/templates/project/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
@@ -1,40 +1,33 @@
 """Project settings."""
 
-# Instantiate and list your custom project hooks here
+# TODO: LINK TO MORE DOCS
+
+# Instantiated project hooks to inject additional behaviour into the execution timeline.
 # from {{cookiecutter.python_package}}.hooks import ProjectHooks
 # HOOKS = (ProjectHooks(),)
 
-# List the installed plugins for which to disable auto-registry
+# Installed plugins for which to disable hook auto-registration.
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
 
-# Define where to store data from a KedroSession. Defaults to BaseSessionStore.
+# Class and constructor keyword arguments that define how and where to store KedroSession data.
 # from kedro.framework.session.store import ShelveStore
 # SESSION_STORE_CLASS = ShelveStore
-
-# Define keyword arguments to be passed to `SESSION_STORE_CLASS` constructor
 # SESSION_STORE_ARGS = {
 #     "path": "./sessions"
 # }
 
-# Define custom context class. Defaults to `KedroContext`
+# Class that manages Kedro's library components.
 # CONTEXT_CLASS = KedroContext
 
-# Define the configuration folder. Defaults to `conf`
+# Directory that holds configuration.
 # CONF_SOURCE = "conf"
 
-# Define the project ConfigLoader class here.
-# Defaults to kedro.config.ConfigLoader
+# Class and constructor keyword argument that define how configuration is loaded.
 # from kedro.config import TemplatedConfigLoader
 # CONFIG_LOADER_CLASS = TemplatedConfigLoader
-
-# Define keyword arguments to be passed to `CONFIG_LOADER_CLASS` constructor.
-# These kwargs depend on the `ConfigLoader` class implementation.
 # CONFIG_LOADER_ARGS = {
 #     "globals_pattern": "*globals.yml",
-#     "base_env": "base",
-#     "default_run_env": "local",
 # }
 
-# Define the project DataCatalog class here.
-# Defaults to kedro.io.DataCatalog
+# Class that manages the Data Catalog.
 # DATA_CATALOG_CLASS = DataCatalog

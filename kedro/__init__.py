35e4ac544a06558e30a41ccd601e37de1afaac14
Lorena Bălan <lorena.balan@quantumblack.com>
2022-03-31 17:02:28 +0100

    Bump up version to 0.18.0 (#1399)

--- kedro/__init__.py
+++ kedro/__init__.py
@@ -3,7 +3,7 @@ data pipelines by providing uniform project templates, data abstraction,
 configuration and pipeline assembly.
 """
 
-__version__ = "0.17.7"
+__version__ = "0.18.0"
 
 
 import logging

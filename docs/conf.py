611fdd08e3a2edcc120465e8e65be109f4b305c3
Deepyaman Datta <deepyaman.datta@utexas.edu>
2022-04-16 08:01:43 -0400

    Bump the `user_agent` to latest version of Firefox
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- docs/conf.py
+++ docs/conf.py
@@ -561,4 +561,4 @@ except Exception as e:
         )
     )
 
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0'
+user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"

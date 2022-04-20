fe07c9c7194c0488d91345b023373f5b6b663afe
Merel Theisen <merel.theisen@quantumblack.com>
2022-04-20 14:46:05 +0200

    Linting
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- features/steps/test_starter/{{ cookiecutter.repo_name }}/src/tests/test_run.py
+++ features/steps/test_starter/{{ cookiecutter.repo_name }}/src/tests/test_run.py
@@ -11,11 +11,10 @@ To run the tests, run ``kedro test`` from the project root directory.
 from pathlib import Path
 
 import pytest
-from kedro.framework.project import settings
-
 from kedro.config import ConfigLoader
 from kedro.framework.context import KedroContext
 from kedro.framework.hooks import _create_hook_manager
+from kedro.framework.project import settings
 
 
 @pytest.fixture

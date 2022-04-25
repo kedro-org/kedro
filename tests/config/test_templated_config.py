9bb68c6f7bbd4fd35cdf0456f389f2b46bb429f8
Merel Theisen <merel.theisen@quantumblack.com>
2022-04-25 16:17:03 +0100

    Linting
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- tests/config/test_templated_config.py
+++ tests/config/test_templated_config.py
@@ -368,12 +368,13 @@ class TestTemplatedConfigLoader:
         assert catalog == expected_catalog
 
     @pytest.mark.usefixtures("proj_catalog_globals", "catalog_with_jinja2_syntax")
-    def test_catalog_with_jinja2_syntax_and_globals_file(self, tmp_path, template_config):
+    def test_catalog_with_jinja2_syntax_and_globals_file(self, tmp_path):
         """Test catalog with jinja2 syntax with globals yaml file"""
         proj_catalog = tmp_path / _DEFAULT_RUN_ENV / "catalog.yml"
         _write_yaml(proj_catalog, {})
         config_loader = TemplatedConfigLoader(
-            str(tmp_path), globals_pattern="*globals.yml",
+            str(tmp_path),
+            globals_pattern="*globals.yml",
         )
         config_loader.default_run_env = ""
         catalog = config_loader.get("catalog*.yml")

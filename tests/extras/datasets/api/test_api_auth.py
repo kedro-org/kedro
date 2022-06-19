82584e689358cefc9202ed8c49a819ced96218ee
Niels Drost <codingdutchman@gmail.com>
2022-06-19 03:59:09 +0200

    removed unnecessary comments
    Signed-off-by: Niels Drost <codingdutchman@gmail.com>

--- tests/extras/datasets/api/test_api_auth.py
+++ tests/extras/datasets/api/test_api_auth.py
@@ -17,11 +17,9 @@ class AccessTokenAuth(AuthBase):
     """Attaches Access Token Authentication to the given Request object."""
 
     def __init__(self, token):
-        # setup any auth-related data here
         self.token = token
 
     def __call__(self, r):
-        # modify and return the request
         r.headers["Authorization"] = f"access_token {self.token}"
         return r
 

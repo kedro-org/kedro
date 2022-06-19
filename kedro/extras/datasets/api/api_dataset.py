a0a74645c9704a17a1ba8feeba9e07f4592bca70
Niels Drost <codingdutchman@gmail.com>
2022-06-19 04:15:43 +0200

    initial version with load args.
    Signed-off-by: Niels Drost <codingdutchman@gmail.com>

--- kedro/extras/datasets/api/api_dataset.py
+++ kedro/extras/datasets/api/api_dataset.py
@@ -1,12 +1,15 @@
 """``APIDataSet`` loads the data from HTTP(S) APIs.
 It uses the python requests library: https://requests.readthedocs.io/en/master/
 """
-from typing import Any, Dict, Iterable, List, Union
+from copy import deepcopy
+from typing import Any, Dict
 
 import requests
-from requests.auth import AuthBase
 
+from kedro.extras.datasets.api.BaseAuthFactory import BaseAuthFactory
 from kedro.io.core import AbstractDataSet, DataSetError
+
+_DEFAULT_CREDENTIALS = {}
 
 
 class APIDataSet(AbstractDataSet):
@@ -21,75 +24,57 @@ class APIDataSet(AbstractDataSet):
         >>>
         >>> data_set = APIDataSet(
         >>>     url="https://quickstats.nass.usda.gov",
-        >>>     params={
-        >>>         "key": "SOME_TOKEN",
-        >>>         "format": "JSON",
-        >>>         "commodity_desc": "CORN",
-        >>>         "statisticcat_des": "YIELD",
-        >>>         "agg_level_desc": "STATE",
-        >>>         "year": 2000
+        >>>     load_args={
+        >>>         "params": {
+        >>>             "key": "SOME_TOKEN",
+        >>>             "format": "JSON",
+        >>>             "commodity_desc": "CORN",
+        >>>             "statisticcat_des": "YIELD",
+        >>>             "agg_level_desc": "STATE",
+        >>>             "year": 2000
+        >>>         }
         >>>     }
         >>> )
         >>> data = data_set.load()
     """
 
-    # pylint: disable=too-many-arguments
     def __init__(
         self,
         url: str,
         method: str = "GET",
-        data: Any = None,
-        params: Dict[str, Any] = None,
-        headers: Dict[str, Any] = None,
-        auth: Union[Iterable[str], AuthBase] = None,
-        json: Union[List, Dict[str, Any]] = None,
-        timeout: int = 60,
-        credentials: Union[Iterable[str], AuthBase] = None,
+        load_args: Dict[str, Any] = None,
+        auth: Dict[str, Any] = None,
+        credentials: Dict[str, Any] = None
     ) -> None:
         """Creates a new instance of ``APIDataSet`` to fetch data from an API endpoint.
 
         Args:
             url: The API URL endpoint.
             method: The Method of the request, GET, POST, PUT, DELETE, HEAD, etc...
-            data: The request payload, used for POST, PUT, etc requests
-                https://requests.readthedocs.io/en/master/user/quickstart/#more-complicated-post-requests
-            params: The url parameters of the API.
-                https://requests.readthedocs.io/en/master/user/quickstart/#passing-parameters-in-urls
-            headers: The HTTP headers.
-                https://requests.readthedocs.io/en/master/user/quickstart/#custom-headers
-            auth: Anything ``requests`` accepts. Normally it's either ``('login', 'password')``,
-                or ``AuthBase``, ``HTTPBasicAuth`` instance for more complex cases. Any
-                iterable will be cast to a tuple.
-            json: The request payload, used for POST, PUT, etc requests, passed in
-                to the json kwarg in the requests object.
-                https://requests.readthedocs.io/en/master/user/quickstart/#more-complicated-post-requests
-            timeout: The wait time in seconds for a response, defaults to 1 minute.
-                https://requests.readthedocs.io/en/master/user/quickstart/#timeouts
-            credentials: same as ``auth``. Allows specifying ``auth`` secrets in
-                credentials.yml.
-
-        Raises:
-            ValueError: if both ``credentials`` and ``auth`` are specified.
+            load_args: Additional parameters to be fed to requests.request.
+                https://docs.python-requests.org/en/latest/api/
+            auth: provide arguments to construct a Requests `BaseAuth` object.
+            credentials: Allows specifying secrets in credentials.yml.
         """
         super().__init__()
 
-        if credentials is not None and auth is not None:
-            raise ValueError("Cannot specify both auth and credentials.")
+        self._credentials = deepcopy(_DEFAULT_CREDENTIALS)
+        if credentials is not None:
+            self._credentials.update(credentials)
 
-        auth = credentials or auth
+        self._auth = None
 
-        if isinstance(auth, Iterable):
-            auth = tuple(auth)
+        if auth is not None:
+            if 'type' not in auth:
+                raise DataSetError("'type' is missing from Authenticator configuration")
+            auth_type = auth.pop('type')
+            self._auth = BaseAuthFactory.create(auth_type, **auth, **credentials)
 
         self._request_args: Dict[str, Any] = {
             "url": url,
             "method": method,
-            "data": data,
-            "params": params,
-            "headers": headers,
-            "auth": auth,
-            "json": json,
-            "timeout": timeout,
+            "auth": self._auth,
+            **load_args
         }
 
     def _describe(self) -> Dict[str, Any]:
@@ -114,5 +99,4 @@ class APIDataSet(AbstractDataSet):
 
     def _exists(self) -> bool:
         response = self._execute_request()
-
         return response.ok

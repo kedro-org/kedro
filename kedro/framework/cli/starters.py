a6a4aae8d647807fec7216f5afa8d78118d0c8b4
Merel Theisen <merel.theisen@quantumblack.com>
2022-07-04 17:35:33 +0100

    Update info message to match correct output
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- kedro/framework/cli/starters.py
+++ kedro/framework/cli/starters.py
@@ -357,7 +357,7 @@ def _create_project(template_path: str, cookiecutter_args: Dict[str, Any]):
     extra_context = cookiecutter_args.get("extra_context", {})
     project_name = extra_context.get("project_name", "")
     python_package = extra_context.get(
-        "python_package", project_name.lower().replace(" ", "_")
+        "python_package", project_name.lower().replace(" ", "_").replace("-", "_")
     )
     click.secho(
         f"\nThe project name '{project_name}' has been applied to: "

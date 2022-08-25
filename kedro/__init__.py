9037c39f3a0f6818664fd6918e65705db46647ed
Antony Milne <antony.milne@quantumblack.com>
2022-08-25 17:31:41 +0100

    Bump version
    Signed-off-by: Antony Milne <antony.milne@quantumblack.com>

--- kedro/__init__.py
+++ kedro/__init__.py
@@ -3,7 +3,7 @@ data pipelines by providing uniform project templates, data abstraction,
 configuration and pipeline assembly.
 """
 
-__version__ = "0.18.2"
+__version__ = "0.18.3b"
 
 
 import logging

c7ca31ee6efbc449e3ad54cbfb0b1fbc49f9d13a
Avaneesh Yembadi <11796986+avan-sh@users.noreply.github.com>
2022-09-10 16:09:23 -0400

    extend docs
    Signed-off-by: Avaneesh Yembadi <11796986+avan-sh@users.noreply.github.com>

--- kedro/framework/session/shelvestore.py
+++ kedro/framework/session/shelvestore.py
@@ -11,7 +11,8 @@ from .store import BaseSessionStore
 
 
 class ShelveStore(BaseSessionStore):
-    """Stores the session data on disk using `shelve` package."""
+    """Stores the session data on disk using `shelve` package.
+    This is an example of how to persist data on disk."""
 
     _lock = Lock()
 

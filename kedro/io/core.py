095b1d957c4c0a45f5646a7ce21f4e0f983ac5f5
Nok Chan <nok.lam.chan@quantumblack.com>
2022-09-26 17:57:54 +0100

    remove cloud protocol
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- kedro/io/core.py
+++ kedro/io/core.py
@@ -681,10 +681,9 @@ def _parse_filepath(filepath: str) -> Dict[str, str]:
     options = {"protocol": protocol, "path": path}
 
     if parsed_path.netloc:
-        if protocol in CLOUD_PROTOCOLS:
-            host_with_port = parsed_path.netloc.rsplit("@", 1)[-1]
-            host = host_with_port.rsplit(":", 1)[0]
-            options["path"] = host + options["path"]
+        host_with_port = parsed_path.netloc.rsplit("@", 1)[-1]
+        host = host_with_port.rsplit(":", 1)[0]
+        options["path"] = host + options["path"]
 
     return options
 

13ba2e35edcbc1e32b91170291f26a8b3a03ffea
Jimmy Stammers <jimmy.stammers@gmail.com>
2022-09-28 09:39:44 +0100

    Fix issue with specifying format for SparkHiveDataSet (#1857)
    Signed-off-by: jstammers <jimmy.stammers@cgastrategy.com>

--- tests/extras/datasets/spark/test_spark_hive_dataset.py
+++ tests/extras/datasets/spark/test_spark_hive_dataset.py
@@ -301,3 +301,14 @@ class TestSparkHiveDataSet:
             r"table_doesnt_exist\], \[\], false\n",
         ):
             dataset.load()
+
+    def test_save_delta_format(self, mocker):
+        dataset = SparkHiveDataSet(
+            database="default_1", table="delta_table", save_args={"format": "delta"}
+        )
+        mocked_save = mocker.patch("pyspark.sql.DataFrameWriter.saveAsTable")
+        dataset.save(_generate_spark_df_one())
+        mocked_save.assert_called_with(
+            "default_1.delta_table", mode="errorifexists", format="delta"
+        )
+        assert dataset._format == "delta"

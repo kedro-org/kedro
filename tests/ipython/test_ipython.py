cdb327b86dc1d4ec88d2a7998e59b899531a5e06
Jannic Holzer <jannic.holzer@quantumblack.com>
2022-10-21 16:29:56 +0100

    Add tests for the case with an unresolvable project
    Signed-off-by: Jannic Holzer <jannic.holzer@quantumblack.com>

--- tests/ipython/test_ipython.py
+++ tests/ipython/test_ipython.py
@@ -271,3 +271,23 @@ class TestProjectPathResolution:
         result = _resolve_project_path()
         expected = Path("/test").resolve()
         assert result == expected
+
+    def test_project_path_unresolvable(self, mocker):
+        mocker.patch(
+            "kedro.ipython._find_kedro_project", return_value=None
+        )
+        result = _resolve_project_path()
+        expected = None
+        assert result == expected
+
+    def test_project_path_unresolvable_warning(self, mocker, caplog, ipython):
+        mocker.patch(
+            "kedro.ipython._find_kedro_project", return_value=None
+        )
+        ipython.magic("load_ext kedro.ipython")
+        log_messages = [record.getMessage() for record in caplog.records]
+        expected_message = (
+            "Kedro extension was registered but couldn't find a Kedro project. "
+            "Make sure you run '%reload_kedro <project_root>'."
+        )
+        assert expected_message in log_messages

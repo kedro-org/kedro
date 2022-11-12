4f0d5acc45b30b831bdcb0e01ba1f324b6cccc66
Nok Chan <nok.lam.chan@quantumblack.com>
2022-11-12 22:49:49 +0000

    Test update
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,36 @@ https://docs.pytest.org/en/latest/fixture.html
 import os
 import sys
 
+import aiobotocore
+import botocore
+import moto
 import pytest
+from botocore.awsrequest import AWSResponse
+
+
+# Patch `aiobotocore.endpoint.convert_to_response_dict` to work with moto.
+class PatchedAWSResponse:
+    def __init__(self, response: botocore.awsrequest.AWSResponse):
+        self._response = response
+        self.status_code = response.status_code
+        self.raw = response.raw
+        self.raw.raw_headers = {}
+
+    @property
+    async def content(self):
+        return self._response.content
+
+
+def factory(original):
+    def patched_convert_to_response_dict(http_response, operation_model):
+        return original(PatchedAWSResponse(http_response), operation_model)
+
+    return patched_convert_to_response_dict
+
+
+aiobotocore.endpoint.convert_to_response_dict = factory(
+    aiobotocore.endpoint.convert_to_response_dict
+)
 
 
 @pytest.fixture(autouse=True)

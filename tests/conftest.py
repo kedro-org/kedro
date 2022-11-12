d31ae1bd31a3dd78aca7e4a61484b0fb0fbcbfa6
Nok Chan <nok.lam.chan@quantumblack.com>
2022-11-12 23:48:28 +0000

    fix the fixture
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,35 +8,71 @@ import os
 import sys
 
 import aiobotocore
+import aiobotocore.endpoint
 import botocore
-import moto
 import pytest
 from botocore.awsrequest import AWSResponse
-import aiobotocore.endpoint
 
-# Patch `aiobotocore.endpoint.convert_to_response_dict` to work with moto.
-class PatchedAWSResponse:
+from typing import Callable
+from unittest.mock import MagicMock
+
+import aiobotocore.awsrequest
+import aiobotocore.endpoint
+import aiohttp
+import aiohttp.client_reqrep
+import aiohttp.typedefs
+import botocore.awsrequest
+import botocore.model
+import pytest
+
+"""
+Patch aiobotocore to work with moto
+See https://github.com/aio-libs/aiobotocore/issues/755
+"""
+
+
+class MockAWSResponse(aiobotocore.awsrequest.AioAWSResponse):
     def __init__(self, response: botocore.awsrequest.AWSResponse):
-        self._response = response
+        self._moto_response = response
         self.status_code = response.status_code
-        self.raw = response.raw
-        self.raw.raw_headers = {}
+        self.raw = MockHttpClientResponse(response)
+
+    # adapt async methods to use moto's response
+    async def _content_prop(self) -> bytes:
+        return self._moto_response.content
+
+    async def _text_prop(self) -> str:
+        return self._moto_response.text
+
+
+class MockHttpClientResponse(aiohttp.client_reqrep.ClientResponse):
+    def __init__(self, response: botocore.awsrequest.AWSResponse):
+
+        async def read(self, n: int = -1) -> bytes:
+            # streaming/range requests. used by s3fs
+            return response.content
+
+        self.content = MagicMock(aiohttp.StreamReader)
+        self.content.read = read
+        self.response = response
 
     @property
-    async def content(self):
-        return self._response.content
+    def raw_headers(self) -> aiohttp.typedefs.RawHeaders:
+        # Return the headers encoded the way that aiobotocore expects them
+        return { k.encode('utf-8') : str(v).encode('utf-8') for k, v in self.response.headers.items() }.items()
 
 
-def factory(original):
-    def patched_convert_to_response_dict(http_response, operation_model):
-        return original(PatchedAWSResponse(http_response), operation_model)
+# @pytest.fixture
+# def patch_aiobotocore():
+def factory(original: Callable) -> Callable:
+    def patched_convert_to_response_dict(
+        http_response: botocore.awsrequest.AWSResponse, operation_model: botocore.model.OperationModel
+    ):
+        return original(MockAWSResponse(http_response), operation_model)
 
     return patched_convert_to_response_dict
 
-
-aiobotocore.endpoint.convert_to_response_dict = factory(
-    aiobotocore.endpoint.convert_to_response_dict
-)
+aiobotocore.endpoint.convert_to_response_dict = factory(aiobotocore.endpoint.convert_to_response_dict)
 
 
 @pytest.fixture(autouse=True)

f9546a1fffd51b369a01705219ed45477941a639
Jo Stichbury <jo_stichbury@mckinsey.com>
2022-11-25 10:12:07 +0000

    Update text_dataset.py

--- kedro/extras/datasets/text/text_dataset.py
+++ kedro/extras/datasets/text/text_dataset.py
@@ -24,10 +24,7 @@ class TextDataSet(AbstractVersionedDataSet[str, str]):
     """``TextDataSet`` loads/saves data from/to a text file using an underlying
     filesystem (e.g.: local, S3, GCS)
 
-    Example adding a catalog entry with
-    `YAML API
-    <https://kedro.readthedocs.io/en/stable/data/\
-        data_catalog.html#use-the-data-catalog-with-the-yaml-api>`_:
+    Example adding a catalog entry with YAML API:
 
     .. code-block:: yaml
 

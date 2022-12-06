27bbd14df6073e3701a859fbc3c90154760d2052
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2022-12-06 13:55:01 +0000

    lint: disable line too long
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- kedro/ipython/__init__.py
+++ kedro/ipython/__init__.py
@@ -62,7 +62,7 @@ def load_ipython_extension(ipython):
 def magic_reload_kedro(line: str, local_ns: Dict[str, Any] = None):
     """
     The `%reload_kedro` IPython line magic.
-    See https://kedro.readthedocs.io/en/stable/notebooks_and_ipython/kedro_and_notebooks.html#reload-kedro-line-magic
+    See https://kedro.readthedocs.io/en/stable/notebooks_and_ipython/kedro_and_notebooks.html#reload-kedro-line-magic # pylint: disable=line-too-long
     for more.
     """
     args = parse_argstring(magic_reload_kedro, line)

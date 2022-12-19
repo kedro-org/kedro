072c7a4a77a6d5332d5128f0d49f24c273a60909
Jannic Holzer <jannic.holzer@quantumblack.com>
2022-12-19 18:27:38 +0000

    Add missing import
    Signed-off-by: Jannic Holzer <jannic.holzer@quantumblack.com>

--- kedro/framework/cli/project.py
+++ kedro/framework/cli/project.py
@@ -21,6 +21,7 @@ from kedro.framework.cli.utils import (
     env_option,
     forward_command,
     python_call,
+    split_string,
     split_node_names,
 )
 from kedro.framework.session import KedroSession

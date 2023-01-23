c6aee49d18050fad4b516ed1c609892153e57a8c
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-01-23 14:52:34 +0000

    Update startup.py
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>

--- kedro/framework/startup.py
+++ kedro/framework/startup.py
@@ -20,7 +20,7 @@ class ProjectMetadata(NamedTuple):
     package_name: str
     project_name: str
     project_path: Path
-    project_version: str = kedro_init_version
+    kedro_init_version: str = project_version
     source_dir: Path
 
 

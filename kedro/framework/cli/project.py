b45f9e7622de845c75c190c448d3435d807e9063
Ahdra Merali <ahdra.merali@quantumblack.com>
2023-02-10 14:45:39 +0000

    Undo merge side-effects
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>

--- kedro/framework/cli/project.py
+++ kedro/framework/cli/project.py
@@ -358,9 +358,6 @@ def activate_nbstripout(
     help=NODE_ARG_HELP,
     callback=split_node_names,
 )
-@click.option(
-    "--runner", "-r", type=str, default=None, multiple=False, help=RUNNER_ARG_HELP
-)
 @click.option("--runner", "-r", type=str, default=None, help=RUNNER_ARG_HELP)
 @click.option("--async", "is_async", is_flag=True, help=ASYNC_ARG_HELP)
 @env_option

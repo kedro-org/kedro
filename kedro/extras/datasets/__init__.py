a01ed80ef2a9771369a5a2ba12e9b4a32e7dcf05
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-04-27 18:23:00 +0200

    Add visible DeprecationWarning for kedro.extras.datasets
    Fix gh-1501.
    
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- kedro/extras/datasets/__init__.py
+++ kedro/extras/datasets/__init__.py
@@ -1,3 +1,12 @@
 """``kedro.extras.datasets`` is where you can find all of Kedro's data connectors.
 These data connectors are implementations of the ``AbstractDataSet``.
 """
+
+from warnings import warn as _warn
+
+_warn(
+    "`kedro.extras.datasets` is deprecated and will be removed in Kedro 0.19, "
+    "install `kedro-datasets` instead by running `pip install kedro-datasets`.",
+    DeprecationWarning,
+    stacklevel=2,
+)

714cd16745ef69f910510f002a7e22d52fdf18f7
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-04-28 09:24:31 -0400

    Fix some imports and missed references
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- kedro/extras/datasets/api/__init__.py
+++ kedro/extras/datasets/api/__init__.py
@@ -1,11 +1,11 @@
-"""``APIDataset`` loads the data from HTTP(S) APIs
+"""``APIDataSet`` loads the data from HTTP(S) APIs
 and returns them into either as string or json Dict.
 It uses the python requests library: https://requests.readthedocs.io/en/latest/
 """
 
-__all__ = ["APIDataSet", "APIDataset"]
+__all__ = ["APIDataSet"]
 
 from contextlib import suppress
 
 with suppress(ImportError):
-    from .api_dataset import APIDataSet, APIDataset
+    from .api_dataset import APIDataSet

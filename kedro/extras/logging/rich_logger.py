810d25be10e5678804b92570a245147babe5b01a
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2023-05-24 11:43:22 +0100

    Apply suggestions from code review
    Co-authored-by: Antony Milne <49395058+antonymilne@users.noreply.github.com>

--- kedro/extras/logging/rich_logger.py
+++ kedro/extras/logging/rich_logger.py
@@ -32,22 +32,21 @@ class RichHandler(rich.logging.RichHandler):
         # fixed on their side at some point, but until then we disable it.
         # See https://github.com/Textualize/rich/issues/2455
 
-        mapped_kwargs = {"suppress": [click, str(Path(sys.executable).parent)]}
+        traceback_install_kwargs = {"suppress": [click, str(Path(sys.executable).parent)]}
 
         # Mapping Arguments from RichHandler's Constructor to rich.traceback.install
+        prefix = "tracebacks_"
         for key, value in kwargs.items():
-            prefix = "tracebacks_"
             if key.startswith(prefix):
                 key_prefix_removed = key[len(prefix) :]
                 if key_prefix_removed == "suppress":
-                    mapped_kwargs[key_prefix_removed] += value
+                    mapped_kwargs[key_prefix_removed].extend(value)
                 else:
                     mapped_kwargs[key_prefix_removed] = value
             elif key in ("locals_max_length", "locals_max_string"):
                 mapped_kwargs[key] = value
 
-        if self.rich_tracebacks:
-            if "DATABRICKS_RUNTIME_VERSION" not in os.environ:
+        if self.rich_tracebacks and "DATABRICKS_RUNTIME_VERSION" not in os.environ:
                 # https://rich.readthedocs.io/en/stable/reference/logging.html?highlight=rich%20handler#rich.logging.RichHandler
                 # Support compatible arguments between RichHandler and rich.traceback.install
                 rich.traceback.install(**mapped_kwargs)

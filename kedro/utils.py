796dadde69632383e3ded17861bd2f725ccad98a
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-06-01 23:55:18 -0400

    Fix lint issues

--- kedro/utils.py
+++ kedro/utils.py
@@ -35,17 +35,17 @@ class DeprecatedClassMeta(type):
     Code implementation copied from https://stackoverflow.com/a/52087847
     """
 
-    def __new__(cls, name, bases, classdict, *args, **kwargs):
+    def __new__(mcs, name, bases, classdict, *args, **kwargs):
         alias = classdict.get("_DeprecatedClassMeta__alias")
 
         if alias is not None:
 
-            def new(cls, *args, **kwargs):
-                alias = getattr(cls, "_DeprecatedClassMeta__alias")
+            def new(mcs, *args, **kwargs):
+                alias = getattr(mcs, "_DeprecatedClassMeta__alias")
 
                 if alias is not None:
                     warn(
-                        f"{cls.__name__} has been renamed to {alias.__name__}, and the "
+                        f"{mcs.__name__} has been renamed to {alias.__name__}, and the "
                         f"alias will be removed Kedro 0.19.0",
                         DeprecationWarning,
                         stacklevel=2,
@@ -58,33 +58,30 @@ class DeprecatedClassMeta(type):
 
         fixed_bases = []
 
-        for b in bases:
-            alias = getattr(b, "_DeprecatedClassMeta__alias", None)
+        for base in bases:
+            alias = getattr(base, "_DeprecatedClassMeta__alias", None)
 
             if alias is not None:
                 warn(
-                    f"{b.__name__} has been renamed to {alias.__name__}, and the alias "
-                    f"will be removed in Kedro 0.19.0",
+                    f"{base.__name__} has been renamed to {alias.__name__}, and the "
+                    f"alias will be removed in Kedro 0.19.0",
                     DeprecationWarning,
                     stacklevel=2,
                 )
 
             # Avoid duplicate base classes.
-            b = alias or b
-            if b not in fixed_bases:
-                fixed_bases.append(b)
+            base = alias or base
+            if base not in fixed_bases:
+                fixed_bases.append(base)
 
         fixed_bases = tuple(fixed_bases)
 
-        return super().__new__(cls, name, fixed_bases, classdict, *args, **kwargs)
+        return super().__new__(mcs, name, fixed_bases, classdict, *args, **kwargs)
 
     def __instancecheck__(cls, instance):
         return any(
-            cls.__subclasscheck__(c) for c in {type(instance), instance.__class__}
+            cls.__subclasscheck__(c) for c in [type(instance), instance.__class__]
         )
 
     def __subclasscheck__(cls, subclass):
-        if subclass is cls:
-            return True
-        else:
-            return issubclass(subclass, getattr(cls, "_DeprecatedClassMeta__alias"))
+        return subclass is cls or issubclass(subclass, getattr(cls, "_DeprecatedClassMeta__alias"))

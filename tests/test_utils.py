452ad0b28d4339d26fd5275a1fbc7a9acb92abc2
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-06-05 00:16:46 -0400

    Add `DeprecatedClassMeta` tests from StackOverflow

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from kedro.utils import load_obj
+from kedro.utils import DeprecatedClassMeta, load_obj
 
 
 # pylint: disable=too-few-public-methods
@@ -28,3 +28,53 @@ class TestExtractObject:
     def test_load_obj_invalid_module(self):
         with pytest.raises(ImportError, match=r"No module named 'missing_path'"):
             load_obj("InvalidClass", "missing_path")
+
+
+class NewClass:
+    foo = 1
+
+
+class NewClassSubclass(NewClass):
+    pass
+
+
+class DeprecatedClass(metaclass=DeprecatedClassMeta):
+    _DeprecatedClassMeta__alias = NewClass
+
+
+class DeprecatedClassSubclass(DeprecatedClass):
+    foo = 2
+
+
+class DeprecatedClassSubSubclass(DeprecatedClassSubclass):
+    foo = 3
+
+
+class TestDeprecatedClassMeta:
+    def test_is_subclass_of_deprecated_class(self):
+        assert issubclass(DeprecatedClass, DeprecatedClass)
+        assert issubclass(DeprecatedClassSubclass, DeprecatedClass)
+        assert issubclass(DeprecatedClassSubSubclass, DeprecatedClass)
+        assert issubclass(NewClass, DeprecatedClass)
+        assert issubclass(NewClassSubclass, DeprecatedClass)
+
+    def test_is_subclass_of_new_class(self):
+        assert issubclass(DeprecatedClassSubclass, NewClass)
+        assert issubclass(DeprecatedClassSubSubclass, NewClass)
+
+    def test_is_instance_of_deprecated_class(self):
+        assert isinstance(DeprecatedClass(), DeprecatedClass)
+        assert isinstance(DeprecatedClassSubclass(), DeprecatedClass)
+        assert isinstance(DeprecatedClassSubSubclass(), DeprecatedClass)
+        assert isinstance(NewClass(), DeprecatedClass)
+        assert isinstance(NewClassSubclass(), DeprecatedClass)
+
+    def test_is_instance_of_new_class(self):
+        assert isinstance(DeprecatedClassSubclass(), NewClass)
+        assert isinstance(DeprecatedClassSubSubclass(), NewClass)
+
+    def test_inheritance(self):
+        assert NewClass().foo == 1
+        assert DeprecatedClass().foo == 1
+        assert DeprecatedClassSubclass().foo == 2
+        assert DeprecatedClassSubSubclass().foo == 3

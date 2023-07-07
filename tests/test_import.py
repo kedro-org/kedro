bbe54158436e746212afc76b7f67d1ebc49a2e9f
Nok <nok.lam.chan@quantumblack.com>
2023-07-07 16:11:12 +0000

    add tests
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,11 +1,32 @@
 import importlib
 import warnings
 import pytest
-from unittest.mock import patch
 
 def test_import_kedro_with_no_official_support_raise_error(mocker, monkeypatch):
     """Test importing kedro should fails with python>=3.11 should fails"""
     import kedro
+    with mocker.patch("kedro.sys"):
+        kedro.__loader__.exec_module(kedro)
+
+@pytest.mark.filterwarnings("default:Kedro")
+def test_import_kedro_success(mocker):
+    """Test importing kedro should fails with python>=3.11 should fails"""
+    import kedro
+    with mocker.patch("kedro.sys"):
+        kedro.__loader__.exec_module(kedro)
+
+def test_import_kedro_success_with_env_variable(mocker, monkeypatch):
+    """Test importing kedro should fails with python>=3.11 should fails"""
+    import kedro
+    monkeypatch.setenv("PYTHONWARNINGS", "default:Kedro")
+    with mocker.patch("kedro.sys"):
+        kedro.__loader__.exec_module(kedro)
+
+def test_import_kedro_success_with_env_variable(mocker, monkeypatch):
+    """Test importing kedro should fails with python>=3.11 should fails"""
+    import kedro
+    with mocker.patch("kedro.sys"):
+        kedro.__loader__.exec_module(kedro)
     
 @pytest.mark.filterwarnings("default:Kedro")
 def test_import_kedro_slient_warning(mocker, monkeypatch):

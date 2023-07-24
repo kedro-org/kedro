dbb7c996b031beff94b51b6dec322db6ca791fad
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-07-23 22:23:35 -0500

    Make validation of unique node outputs much faster

--- kedro/pipeline/node.py
+++ kedro/pipeline/node.py
@@ -480,11 +480,12 @@ class Node:
                 ) from exc
 
     def _validate_unique_outputs(self):
-        diff = Counter(self.outputs) - Counter(set(self.outputs))
+        c = Counter(self.outputs)
+        diff = {k for k in c if c[k] > 1}
         if diff:
             raise ValueError(
-                f"Failed to create node {self} due to duplicate"
-                f" output(s) {set(diff.keys())}.\nNode outputs must be unique."
+                f"Failed to create node {self} due to duplicate "
+                f"output(s) {diff}.\nNode outputs must be unique."
             )
 
     def _validate_inputs_dif_than_outputs(self):

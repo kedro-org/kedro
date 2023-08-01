bd20c077b9bab3ff6589a467f66101fd1ab9ce74
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-08-01 13:15:00 +0100

    test docsbuild on merels branch
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ extras_require = {
         "sphinxcontrib-mermaid~=0.7.1",
         "myst-parser~=1.0.0",
         "Jinja2<3.1.0",
-        "kedro-datasets[all,pandas-deltatabledataset]@git+https://github.com/kedro-org/kedro-plugins@bugfix/deltatabledataset-indent-fix#subdirectory=kedro-datasets",
+        "kedro-datasets[all,pandas-deltatabledataset]@git+https://github.com/kedro-org/kedro-plugins@fix-deltatableset-indentation#subdirectory=kedro-datasets",
     ],
     "geopandas": _collect_requirements(geopandas_require),
     "matplotlib": _collect_requirements(matplotlib_require),

286f7e6f4345fde07f38e7ebd83c6c5e238b8373
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-08-03 12:21:46 +0100

    pin spark < 3.4 as its breaking
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 # at least 1.3 to be able to use XMLDataSet and pandas integration with fsspec
 PANDAS = "pandas~=1.3"
-SPARK = "pyspark>=2.2, <4.0"
+SPARK = "pyspark>=2.2, <3.4"
 HDFS = "hdfs>=2.5.8, <3.0"
 S3FS = "s3fs>=0.3.0, <0.5"
 

68025491b17a4d35be84b4eadc04c720fbb69314
lrcouto <laurarccouto@gmail.com>
2023-08-24 12:31:09 -0300

    Fix linter error
    Signed-off-by: lrcouto <laurarccouto@gmail.com>

--- kedro/templates/project/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
+++ kedro/templates/project/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
@@ -23,6 +23,7 @@ https://kedro.readthedocs.io/en/stable/kedro_project_setup/settings.html."""
 
 # Class that manages how configuration is loaded.
 from kedro.config import OmegaConfigLoader
+
 CONFIG_LOADER_CLASS = OmegaConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 # CONFIG_LOADER_ARGS = {

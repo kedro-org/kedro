592afb5fe7ed8e417f697776bf407f06a122fba3
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-08-31 16:37:21 +0100

    Update starters.py
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>

--- kedro/framework/cli/starters.py
+++ kedro/framework/cli/starters.py
@@ -232,8 +232,6 @@ def new(config_path, starter_alias, checkout, directory, **kwargs):
     else:
         config = _fetch_config_from_user_prompts(prompts_required, cookiecutter_context)
         selected_add_ons = config.get("add_ons", "none").split(',')
-        print("-----------------------------------------------")
-        print(selected_add_ons)
 
     cookiecutter_args = _make_cookiecutter_args(config, checkout, directory)
     _create_project(template_path, cookiecutter_args)

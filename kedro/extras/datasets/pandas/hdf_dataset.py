cb9a0cde5fbe6fa316f0844f49e254f0eee81960
L. R. Couto <57910428+lrcouto@users.noreply.github.com>
2023-09-01 11:02:55 -0300

    Fix docstrings on kedro/extras/datasets (#2995)
    * Fix docstrings on kedro/extras/datasets
    
    Signed-off-by: lrcouto <laurarccouto@gmail.com>
    
    * Fix formatting error
    
    Signed-off-by: lrcouto <laurarccouto@gmail.com>
    
    * Explicitly define code block
    
    Signed-off-by: lrcouto <laurarccouto@gmail.com>
    
    * Fix empty line under code block
    
    Signed-off-by: lrcouto <laurarccouto@gmail.com>
    
    * Fix formatting error
    
    Signed-off-by: lrcouto <laurarccouto@gmail.com>
    
    * Fix broken link
    
    Signed-off-by: lrcouto <laurarccouto@gmail.com>
    
    * Bump kedro-datasets version
    
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    
    * Update links in partitioned and incremental datasets
    
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    
    * Update links in partitioned and incremental datasets
    
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    
    * Update links in partitioned dataset
    
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    
    * Update links in partitioned dataset
    
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    
    * Add polars.GenericDataSet to .rst
    
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    
    ---------
    
    Signed-off-by: lrcouto <laurarccouto@gmail.com>
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    Co-authored-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>
    Signed-off-by: Jason Hite <jasonmhite@gmail.com>

--- kedro/extras/datasets/pandas/hdf_dataset.py
+++ kedro/extras/datasets/pandas/hdf_dataset.py
@@ -30,7 +30,8 @@ class HDFDataSet(AbstractVersionedDataset[pd.DataFrame, pd.DataFrame]):
 
     Example usage for the
     `YAML API <https://kedro.readthedocs.io/en/stable/data/\
-    data_catalog.html#use-the-data-catalog-with-the-yaml-api>`_:
+    data_catalog_yaml_examples.html>`_:
+
 
     .. code-block:: yaml
 
@@ -42,7 +43,7 @@ class HDFDataSet(AbstractVersionedDataset[pd.DataFrame, pd.DataFrame]):
 
     Example usage for the
     `Python API <https://kedro.readthedocs.io/en/stable/data/\
-    data_catalog.html#use-the-data-catalog-with-the-code-api>`_:
+    advanced_data_catalog_usage.html>`_:
     ::
 
         >>> from kedro.extras.datasets.pandas import HDFDataSet

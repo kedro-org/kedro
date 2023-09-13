bb4e3589c5bab9cfea61e60983a5a6d62dc3e9bd
Ahdra Merali <ahdra.merali@quantumblack.com>
2023-09-13 16:54:11 +0100

    Ensure add-ons message only shows when add-ons are configurable
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>

--- kedro/framework/cli/starters.py
+++ kedro/framework/cli/starters.py
@@ -364,10 +364,13 @@ def _create_project(template_path: str, cookiecutter_args: dict[str, Any]):
         "python_package", project_name.lower().replace(" ", "_").replace("-", "_")
     )
     add_ons = extra_context.get("add_ons")
-    if add_ons == "none":
-        click.secho("\nYou have selected no add-ons")
-    else:
-        click.secho(f"\nYou have selected the following add-ons: '{add_ons}'")
+
+    # Only non-starter projects have configurable add-ons
+    if template_path == str(TEMPLATE_PATH):
+        if add_ons == "none":
+            click.secho("\nYou have selected no add-ons")
+        else:
+            click.secho(f"\nYou have selected the following add-ons: '{add_ons}'")
 
     click.secho(
         f"\nThe project name '{project_name}' has been applied to: "

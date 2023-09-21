50187ffa963d5b4ab21edce5cba0e72fb85c3998
Nok Chan <nok.lam.chan@quantumblack.com>
2023-09-21 17:17:24 +0100

    Remove kedro.extras.datasets and kedro.extras
    Signed-off-by: Nok Chan <nok.lam.chan@quantumblack.com>

--- kedro/extras/datasets/json/__init__.py
+++
@@ -1,8 +0,0 @@
-"""``AbstractDataset`` implementation to load/save data from/to a JSON file."""
-
-__all__ = ["JSONDataSet"]
-
-from contextlib import suppress
-
-with suppress(ImportError):
-    from .json_dataset import JSONDataSet

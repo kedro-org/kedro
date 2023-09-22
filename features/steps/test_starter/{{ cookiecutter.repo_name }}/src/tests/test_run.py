2aef57196599d62a99a76d34d5d1c95442eed23a
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2023-09-22 18:01:46 +0100

    Remove Deprecated development  CLI (#3065)
    * Remove nbstripout
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove build-reqs
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove build-docs
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove kedro lint
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove kedro test
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove kedro jupyter convert
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove jupyter convert e2e
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove test TestActivateNbstripoutCommand
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove KedroTestCommand
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove KedroLintCommand
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove test KedroBuildDoc
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove TestKedroBuildReq
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove TestConvertNotebookCommand:
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Clean up leftover from removed test
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove TestExportNodes
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Lint
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove leftover
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove build_doc e2e
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove kedro test e2e
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove kedro build-reqs e2e
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove activate_nbstripout.feature
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Remove build-reqs e2e
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Clean up cli reference in commands_reference.md
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove node convertion session
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * update reference to CLI command
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Fix unit tests
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * remove link from faq
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    ---------
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>

--- features/steps/test_starter/{{ cookiecutter.repo_name }}/src/tests/test_run.py
+++ features/steps/test_starter/{{ cookiecutter.repo_name }}/src/tests/test_run.py
@@ -5,7 +5,7 @@ Tests should be placed in ``src/tests``, in modules that mirror your
 project's structure, and in files named test_*.py. They are simply functions
 named ``test_*`` which test a unit of logic.
 
-To run the tests, run ``kedro test`` from the project root directory.
+To run the tests, run ``pytest`` from the project root directory.
 """
 
 from pathlib import Path

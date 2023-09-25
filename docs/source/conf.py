79b791c831067dd2b0084b5093563a1a4b6c070b
Ahdra Merali <ahdra.merali@quantumblack.com>
2023-09-25 21:30:57 +0100

    Fix broken link
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -136,7 +136,6 @@ type_targets = {
         "CONF_SOURCE",
         "integer -- return number of occurrences of value",
         "integer -- return first index of value.",
-        "kedro_datasets.pandas.csv_dataset.CSVDataSet",
         "kedro_datasets.pandas.json_dataset.JSONDataSet",
         "pluggy._manager.PluginManager",
         "PluginManager",

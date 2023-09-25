b1c1bd964994e3777ab79bcccddfad023dd13bf8
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2023-09-25 12:58:18 +0100

    Remove some requirements from setup.py
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- setup.py
+++ setup.py
@@ -23,65 +23,62 @@ def _collect_requirements(requires):
     return sorted(set(chain.from_iterable(requires.values())))
 
 
-api_require = {"api.APIDataSet": ["requests~=2.20"]}
-biosequence_require = {"biosequence.BioSequenceDataSet": ["biopython~=1.73"]}
-dask_require = {"dask.ParquetDataSet": ["dask[complete]~=2021.10", "triad>=0.6.7, <1.0"]}
-geopandas_require = {
-    "geopandas.GeoJSONDataSet": ["geopandas>=0.6.0, <1.0", "pyproj~=3.0"]
-}
-matplotlib_require = {"matplotlib.MatplotlibWriter": ["matplotlib>=3.0.3, <4.0"]}
-holoviews_require = {"holoviews.HoloviewsWriter": ["holoviews>=1.13.0"]}
-networkx_require = {"networkx.NetworkXDataSet": ["networkx~=2.4"]}
-pandas_require = {
-    "pandas.CSVDataSet": [PANDAS],
-    "pandas.ExcelDataSet": [PANDAS, "openpyxl>=3.0.6, <4.0"],
-    "pandas.FeatherDataSet": [PANDAS],
-    "pandas.GBQTableDataSet": [PANDAS, "pandas-gbq>=0.12.0, <0.18.0"],
-    "pandas.GBQQueryDataSet": [PANDAS, "pandas-gbq>=0.12.0, <0.18.0"],
-    "pandas.HDFDataSet": [
-        PANDAS,
-        "tables~=3.6.0; platform_system == 'Windows'",
-        "tables~=3.6; platform_system != 'Windows'",
-    ],
-    "pandas.JSONDataSet": [PANDAS],
-    "pandas.ParquetDataSet": [PANDAS, "pyarrow>=1.0, <7.0"],
-    "pandas.SQLTableDataSet": [PANDAS, "SQLAlchemy~=1.2"],
-    "pandas.SQLQueryDataSet": [PANDAS, "SQLAlchemy~=1.2"],
-    "pandas.XMLDataSet": [PANDAS, "lxml~=4.6"],
-    "pandas.GenericDataSet": [PANDAS],
-}
-pickle_require = {"pickle.PickleDataSet": ["compress-pickle[lz4]~=2.1.0"]}
-pillow_require = {"pillow.ImageDataSet": ["Pillow~=9.0"]}
-video_require = {
-    "video.VideoDataSet": ["opencv-python~=4.5.5.64"]
-}
-plotly_require = {
-    "plotly.PlotlyDataSet": [PANDAS, "plotly>=4.8.0, <6.0"],
-    "plotly.JSONDataSet": ["plotly>=4.8.0, <6.0"],
-}
-redis_require = {"redis.PickleDataSet": ["redis~=4.1"]}
-spark_require = {
-    "spark.SparkDataSet": [SPARK, HDFS, S3FS],
-    "spark.SparkHiveDataSet": [SPARK, HDFS, S3FS],
-    "spark.SparkJDBCDataSet": [SPARK, HDFS, S3FS],
-    "spark.DeltaTableDataSet": [SPARK, HDFS, S3FS, "delta-spark>=1.0, <3.0"],
-}
-svmlight_require = {"svmlight.SVMLightDataSet": ["scikit-learn~=1.0.2", "scipy~=1.7.3"]}
-tensorflow_required = {
-    "tensorflow.TensorflowModelDataset": [
-        # currently only TensorFlow V2 supported for saving and loading.
-        # V1 requires HDF5 and serialises differently
-        "tensorflow~=2.0; platform_system != 'Darwin' or platform_machine != 'arm64'",
-        # https://developer.apple.com/metal/tensorflow-plugin/
-        "tensorflow-macos~=2.0; platform_system == 'Darwin' and platform_machine == 'arm64'",
-    ]
-}
-yaml_require = {"yaml.YAMLDataSet": [PANDAS, "PyYAML>=4.2, <7.0"]}
+# api_require = {"api.APIDataSet": ["requests~=2.20"]}
+# biosequence_require = {"biosequence.BioSequenceDataSet": ["biopython~=1.73"]}
+# dask_require = {"dask.ParquetDataSet": ["dask[complete]~=2021.10", "triad>=0.6.7, <1.0"]}
+# geopandas_require = {
+#     "geopandas.GeoJSONDataSet": ["geopandas>=0.6.0, <1.0", "pyproj~=3.0"]
+# }
+# matplotlib_require = {"matplotlib.MatplotlibWriter": ["matplotlib>=3.0.3, <4.0"]}
+# holoviews_require = {"holoviews.HoloviewsWriter": ["holoviews>=1.13.0"]}
+# networkx_require = {"networkx.NetworkXDataSet": ["networkx~=2.4"]}
+# pandas_require = {
+#     "pandas.CSVDataSet": [PANDAS],
+#     "pandas.ExcelDataSet": [PANDAS, "openpyxl>=3.0.6, <4.0"],
+#     "pandas.FeatherDataSet": [PANDAS],
+#     "pandas.GBQTableDataSet": [PANDAS, "pandas-gbq>=0.12.0, <0.18.0"],
+#     "pandas.GBQQueryDataSet": [PANDAS, "pandas-gbq>=0.12.0, <0.18.0"],
+#     "pandas.HDFDataSet": [
+#         PANDAS,
+#         "tables~=3.6.0; platform_system == 'Windows'",
+#         "tables~=3.6; platform_system != 'Windows'",
+#     ],
+#     "pandas.JSONDataSet": [PANDAS],
+#     "pandas.ParquetDataSet": [PANDAS, "pyarrow>=1.0, <7.0"],
+#     "pandas.SQLTableDataSet": [PANDAS, "SQLAlchemy~=1.2"],
+#     "pandas.SQLQueryDataSet": [PANDAS, "SQLAlchemy~=1.2"],
+#     "pandas.XMLDataSet": [PANDAS, "lxml~=4.6"],
+#     "pandas.GenericDataSet": [PANDAS],
+# }
+# pickle_require = {"pickle.PickleDataSet": ["compress-pickle[lz4]~=2.1.0"]}
+# pillow_require = {"pillow.ImageDataSet": ["Pillow~=9.0"]}
+# video_require = {
+#     "video.VideoDataSet": ["opencv-python~=4.5.5.64"]
+# }
+# plotly_require = {
+#     "plotly.PlotlyDataSet": [PANDAS, "plotly>=4.8.0, <6.0"],
+#     "plotly.JSONDataSet": ["plotly>=4.8.0, <6.0"],
+# }
+# redis_require = {"redis.PickleDataSet": ["redis~=4.1"]}
+# spark_require = {
+#     "spark.SparkDataSet": [SPARK, HDFS, S3FS],
+#     "spark.SparkHiveDataSet": [SPARK, HDFS, S3FS],
+#     "spark.SparkJDBCDataSet": [SPARK, HDFS, S3FS],
+#     "spark.DeltaTableDataSet": [SPARK, HDFS, S3FS, "delta-spark>=1.0, <3.0"],
+# }
+# svmlight_require = {"svmlight.SVMLightDataSet": ["scikit-learn~=1.0.2", "scipy~=1.7.3"]}
+# tensorflow_required = {
+#     "tensorflow.TensorflowModelDataset": [
+#         # currently only TensorFlow V2 supported for saving and loading.
+#         # V1 requires HDF5 and serialises differently
+#         "tensorflow~=2.0; platform_system != 'Darwin' or platform_machine != 'arm64'",
+#         # https://developer.apple.com/metal/tensorflow-plugin/
+#         "tensorflow-macos~=2.0; platform_system == 'Darwin' and platform_machine == 'arm64'",
+#     ]
+# }
+# yaml_require = {"yaml.YAMLDataSet": [PANDAS, "PyYAML>=4.2, <7.0"]}
 
 extras_require = {
-    "api": _collect_requirements(api_require),
-    "biosequence": _collect_requirements(biosequence_require),
-    "dask": _collect_requirements(dask_require),
     "docs": [
         # docutils>=0.17 changed the HTML
         # see https://github.com/readthedocs/sphinx_rtd_theme/issues/1115
@@ -99,37 +96,7 @@ extras_require = {
         "Jinja2<3.1.0",
         "kedro-datasets[all]~=1.7.0",
     ],
-    "geopandas": _collect_requirements(geopandas_require),
-    "matplotlib": _collect_requirements(matplotlib_require),
-    "holoviews": _collect_requirements(holoviews_require),
-    "networkx": _collect_requirements(networkx_require),
-    "pandas": _collect_requirements(pandas_require),
-    "pickle": _collect_requirements(pickle_require),
-    "pillow": _collect_requirements(pillow_require),
-    "video": _collect_requirements(video_require),
-    "plotly": _collect_requirements(plotly_require),
-    "redis": _collect_requirements(redis_require),
-    "spark": _collect_requirements(spark_require),
-    "svmlight": _collect_requirements(svmlight_require),
-    "tensorflow": _collect_requirements(tensorflow_required),
-    "yaml": _collect_requirements(yaml_require),
-    **api_require,
-    **biosequence_require,
-    **dask_require,
-    **geopandas_require,
-    **matplotlib_require,
-    **holoviews_require,
-    **networkx_require,
-    **pandas_require,
-    **pickle_require,
-    **pillow_require,
-    **video_require,
-    **plotly_require,
-    **spark_require,
-    **svmlight_require,
-    **tensorflow_required,
-    **yaml_require,
-}
+    }
 
 extras_require["all"] = _collect_requirements(extras_require)
 extras_require["test"] = [
@@ -137,7 +104,6 @@ extras_require["test"] = [
     "adlfs~=2023.1; python_version >= '3.8'",
     "bandit>=1.6.2, <2.0",
     "behave==1.2.6",
-    "biopython~=1.73",
     "blacken-docs==1.9.2",
     "black~=22.0",
     "compress-pickle[lz4]~=2.1.0",
@@ -167,14 +133,6 @@ extras_require["test"] = [
     "memory_profiler>=0.50.0, <1.0",
     "moto==1.3.7; python_version < '3.10'",
     "moto==4.1.12; python_version >= '3.10'",
-    "networkx~=2.4",
-    "opencv-python~=4.5.5.64",
-    "openpyxl>=3.0.3, <4.0",
-    "pandas-gbq>=0.12.0, <0.18.0; python_version < '3.11'",
-    "pandas-gbq>=0.18.0; python_version >= '3.11'",
-    "pandas~=1.3  # 1.3 for read_xml/to_xml",
-    "Pillow~=9.0",
-    "plotly>=4.8.0, <6.0",
     "pre-commit>=2.9.2, <3.0",  # The hook `mypy` requires pre-commit version 2.9.2.
     "pyarrow>=1.0; python_version < '3.11'",
     "pyarrow>=7.0; python_version >= '3.11'",  # Adding to avoid numpy build errors
@@ -193,16 +151,8 @@ extras_require["test"] = [
     "scikit-learn>=1.0.2,<2",
     "scipy>=1.7.3",
     "semver",
-    "SQLAlchemy~=1.2",
-    "tables~=3.6.0; platform_system == 'Windows' and python_version<'3.8'",
-    "tables~=3.8.0; platform_system == 'Windows' and python_version>='3.8'",  # Import issues with python 3.8 with pytables pinning to 3.8.0 fixes this https://github.com/PyTables/PyTables/issues/933#issuecomment-1555917593
-    "tables~=3.6; platform_system != 'Windows'",
-    "tensorflow~=2.0; platform_system != 'Darwin' or platform_machine != 'arm64'",
-    # https://developer.apple.com/metal/tensorflow-plugin/
-    "tensorflow-macos~=2.0; platform_system == 'Darwin' and platform_machine == 'arm64'",
     "triad>=0.6.7, <1.0",
     "trufflehog~=2.1",
-    "xlsxwriter~=1.0",
 ]
 
 setup(

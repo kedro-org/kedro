0aa7100ef2cb2645a4f4b5407f532f56ae0e0b81
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2023-09-26 13:07:14 +0100

    Revert to setup.py
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

---
+++ setup.py
@@ -0,0 +1,19 @@
+from glob import glob
+
+from setuptools import setup
+
+template_files = []
+for pattern in ["**/*", "**/.*", "**/.*/**", "**/.*/.**"]:
+    template_files.extend(
+        [
+            name.replace("kedro/", "", 1)
+            for name in glob("kedro/templates/" + pattern, recursive=True)
+        ]
+    )
+
+
+setup(
+    package_data={
+        "kedro": ["py.typed"] + template_files
+    },
+)

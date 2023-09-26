d28b0bc49bcc6d404d46745a821335420922530d
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2023-09-26 16:12:59 +0100

    Fix unit test
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- tests/io/test_data_catalog.py
+++ tests/io/test_data_catalog.py
@@ -4,7 +4,6 @@ from copy import deepcopy
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import Any
-from unittest.mock import patch
 
 import pandas as pd
 import pytest
@@ -763,19 +762,20 @@ class TestDataCatalogVersioned:
         assert "ds3__csv" in catalog.datasets.__dict__
         assert "jalapeño" in catalog.datasets.__dict__
 
-    def test_no_versions_with_cloud_protocol(self, monkeypatch):
+    def test_no_versions_with_cloud_protocol(self, monkeypatch, mocker):
         """Check the error if no versions are available for load from cloud storage"""
         monkeypatch.setenv("AWS_ACCESS_KEY_ID", "dummmy")
         monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "dummmy")
         version = Version(load=None, save=None)
+        mocker.patch("kedro_datasets.pandas.csv_dataset.fsspec")
         versioned_dataset = CSVDataSet("s3://bucket/file.csv", version=version)
         pattern = re.escape(
             f"Did not find any versions for {versioned_dataset}. "
             f"This could be due to insufficient permission."
         )
-        with patch("kedro_datasets.pandas.CSVDataSet._glob_function", return_value=[]):
-            with pytest.raises(DatasetError, match=pattern):
-                versioned_dataset.load()
+
+        with pytest.raises(DatasetError, match=pattern):
+            versioned_dataset.load()
 
 
 class TestDataCatalogDatasetFactories:

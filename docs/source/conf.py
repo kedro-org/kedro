5b32213d27ba2c3635735312bdf18c2317250cff
Jo Stichbury <jo_stichbury@mckinsey.com>
2023-10-04 15:21:53 +0100

    complete the mission
    Signed-off-by: Jo Stichbury <jo_stichbury@mckinsey.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,7 @@ extensions = [
     "sphinxcontrib.mermaid",
     "myst_parser",
     "notfound.extension",
+    "nbsphinx",
 ]
 
 # enable autosummary plugin  (table of contents for modules/classes/class

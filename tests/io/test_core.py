01e0d1ab674a9a0a5c560074f8920c43a8209d49
Ahdra Merali <ahdra.merali@quantumblack.com>
2023-10-13 12:19:00 +0100

    Add tests (failing)
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>

--- tests/io/test_core.py
+++ tests/io/test_core.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from decimal import Decimal
 from fractions import Fraction
-from pathlib import PurePosixPath
+from pathlib import PurePosixPath, Path
 from typing import Any
 
 import pandas as pd
@@ -13,6 +13,7 @@ from kedro.io.core import (
     AbstractVersionedDataset,
     DatasetError,
     Version,
+    generate_timestamp,
     get_filepath_str,
     get_protocol_and_path,
     validate_on_forbidden_chars,
@@ -37,17 +38,23 @@ FALSE_BUILTINS: list[Any] = [
 
 
 class MyDataset(AbstractDataset):
-    def __init__(self, var=None):
+    def __init__(self, filepath="", save_args=None, fs_args=None, var=None):
+        self._filepath = PurePosixPath(filepath)
+        self.save_args = save_args
+        self.fs_args = fs_args
         self.var = var
 
     def _describe(self):
-        return {"var": self.var}
+        return {"filepath": self._filepath, "var": self.var}
+
+    def _exists(self) -> bool:
+        return Path(self._filepath.as_posix()).exists()
 
     def _load(self):
-        pass  # pragma: no cover
+        return pd.read_csv(self._filepath)
 
-    def _save(self, data):
-        pass  # pragma: no cover
+    def _save(self, data : pd.DataFrame):
+        data.to_csv(str(self._filepath))
 
 
 class MyVersionedDataset(AbstractVersionedDataset):
@@ -63,16 +70,66 @@ class MyVersionedDataset(AbstractVersionedDataset):
         data.to_csv(str(save_path))
 
     def _describe(self):
-        return dict(version=self._version)
+        return dict(filepath=self._filepath, version=self._version)
+
+    def _exists(self) -> bool:
+        return Path(self._filepath.as_posix()).exists()
+
+    # def _exists(self) -> bool:
+        # try:
+        #     path = self._get_load_path()
+        #     load_path = get_filepath_str(path, self._protocol)
+        # except DatasetError:
+        #     return False
+
+@pytest.fixture(params=[None])
+def load_version(request):
+    return request.param
+
+
+@pytest.fixture(params=[None])
+def save_version(request):
+    return request.param or generate_timestamp()
+
+@pytest.fixture(params=[None])
+def load_args(request):
+    return request.param
+
+
+@pytest.fixture(params=[None])
+def save_args(request):
+    return request.param
+
+
+@pytest.fixture(params=[None])
+def fs_args(request):
+    return request.param
+
+@pytest.fixture
+def filepath_versioned(tmp_path):
+    return (tmp_path / "test.csv").as_posix()
+
+@pytest.fixture
+def my_dataset(filepath_versioned, save_args, fs_args):
+    return MyDataset(filepath=filepath_versioned, save_args=save_args, fs_args=fs_args)
+
+@pytest.fixture
+def my_versioned_dataset(filepath_versioned, load_version, save_version):
+    return MyVersionedDataset(filepath=filepath_versioned, version=Version(load_version, save_version))
+
+@pytest.fixture
+def dummy_data():
+    return pd.DataFrame({"col1": [1, 2], "col2": [4, 5], "col3": [5, 6]})
 
 
 class TestCoreFunctions:
     @pytest.mark.parametrize("var", [1, True] + FALSE_BUILTINS)
     def test_str_representation(self, var):
-        assert str(MyDataset(var)) == f"MyDataset(var={var})"
+        filepath = "."
+        assert str(MyDataset(var=var)) == f"MyDataset(filepath={filepath}, var={var})"
 
     def test_str_representation_none(self):
-        assert str(MyDataset()) == "MyDataset()"
+        assert str(MyDataset()) == "MyDataset(filepath=.)"
 
     def test_get_filepath_str(self):
         path = get_filepath_str(PurePosixPath("example.com/test.csv"), "http")
@@ -135,4 +192,84 @@ class TestAbstractDataSet:
 
 
 class TestAbstractVersionedDataset:
-    pass
+    def test_version_str_repr(self, load_version, save_version):
+        """Test that version is in string representation of the class instance
+        when applicable."""
+        filepath = "test.csv"
+        ds_versioned = MyVersionedDataset(
+            filepath=filepath, version=Version(load_version, save_version)
+        )
+
+        assert filepath in str(ds_versioned)
+        ver_str = f"version=Version(load={load_version}, save='{save_version}')"
+        assert ver_str in str(ds_versioned)
+        assert "MyVersionedDataset" in str(ds_versioned)
+
+    def test_save_and_load(self, my_versioned_dataset, dummy_data):
+        """Test that saved and reloaded data matches the original one for
+        the versioned data set."""
+        my_versioned_dataset.save(dummy_data)
+        reloaded = my_versioned_dataset.load()
+        assert dummy_data == reloaded
+
+    def test_no_versions(self, my_versioned_dataset):
+        """Check the error if no versions are available for load."""
+        pattern = r"Did not find any versions for MyVersionedDataset\(.+\)"
+        with pytest.raises(DatasetError, match=pattern):
+            my_versioned_dataset.load()
+
+    def test_exists(self, my_versioned_dataset, dummy_data):
+        """Test `exists` method invocation for versioned data set."""
+        assert not my_versioned_dataset.exists()
+        my_versioned_dataset.save(dummy_data)
+        assert my_versioned_dataset.exists()
+
+    def test_prevent_overwrite(self, my_versioned_dataset, dummy_data):
+        """Check the error when attempting to override the data set if the
+        corresponding json file for a given save version already exists."""
+        my_versioned_dataset.save(dummy_data)
+        pattern = (
+            r"Save path \'.+\' for MyVersionedDataset\(.+\) must "
+            r"not exist if versioning is enabled\."
+        )
+        with pytest.raises(DatasetError, match=pattern):
+            my_versioned_dataset.save(dummy_data)
+
+    @pytest.mark.parametrize(
+        "load_version", ["2019-01-01T23.59.59.999Z"], indirect=True
+    )
+    @pytest.mark.parametrize(
+        "save_version", ["2019-01-02T00.00.00.000Z"], indirect=True
+    )
+    def test_save_version_warning(
+        self, my_versioned_dataset, load_version, save_version, dummy_data
+    ):
+        """Check the warning when saving to the path that differs from
+        the subsequent load path."""
+        pattern = (
+            f"Save version '{save_version}' did not match "
+            f"load version '{load_version}' for "
+            r"MyVersionedDataset\(.+\)"
+        )
+        with pytest.warns(UserWarning, match=pattern):
+            my_versioned_dataset.save(dummy_data)
+
+    def test_versioning_existing_dataset(
+        self, my_dataset, my_versioned_dataset, dummy_data
+    ):
+        """Check the error when attempting to save a versioned dataset on top of an
+        already existing (non-versioned) dataset."""
+        my_dataset.save(dummy_data)
+        assert my_dataset.exists()
+        assert my_dataset._filepath == my_versioned_dataset._filepath
+        pattern = (
+            f"(?=.*file with the same name already exists in the directory)"
+            f"(?=.*{my_versioned_dataset._filepath.parent.as_posix()})"
+        )
+        with pytest.raises(DatasetError, match=pattern):
+            my_versioned_dataset.save(dummy_data)
+
+        # Remove non-versioned dataset and try again
+        Path(my_dataset._filepath.as_posix()).unlink()
+        my_versioned_dataset.save(dummy_data)
+        assert my_versioned_dataset.exists()

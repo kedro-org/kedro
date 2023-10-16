d369f47da1ef1c91f4b03be81906685eb53d9042
Nok <nok.lam.chan@quantumblack.com>
2023-10-16 16:42:44 +0000

    Fix linting
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>

--- features/environment.py
+++ features/environment.py
@@ -105,7 +105,7 @@ def _setup_minimal_env(context):
             "-U",
             # pip==23.2 breaks pip-tools<7.0, and pip-tools>=7.0 does not support Python 3.7
             # pip==23.3 breaks dependency resolution
-            "pip>=21.2,<23.2"
+            "pip>=21.2,<23.2",
         ],
         env=context.env,
     )

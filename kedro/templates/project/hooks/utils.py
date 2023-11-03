406ab93f45b96196babcf4781980a7dd5834f368
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-11-03 10:47:05 +0000

    add docstrings to helper methods and make them private
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>

--- kedro/templates/project/hooks/utils.py
+++ kedro/templates/project/hooks/utils.py
@@ -4,17 +4,26 @@ import toml
 
 current_dir = Path.cwd()
 
-lint_requirements = "black~=22.0\nruff~=0.0.290\n"
-lint_pyproject_requirements = ["tool.ruff"]
+# Requirements for linting tools
+lint_requirements = "black~=22.0\nruff~=0.0.290\n"  # For requirements.txt
+lint_pyproject_requirements = ["tool.ruff"]  # For pyproject.toml
 
-test_requirements = "pytest-cov~=3.0\npytest-mock>=1.7.1, <2.0\npytest~=7.2"
-test_pyproject_requirements = ["tool.pytest.ini_options", "tool.coverage.report"]
+# Requirements and configurations for testing tools and coverage reporting
+test_requirements = "pytest-cov~=3.0\npytest-mock>=1.7.1, <2.0\npytest~=7.2"  # For requirements.txt
+test_pyproject_requirements = ["tool.pytest.ini_options", "tool.coverage.report"]  # For pyproject.toml
 
-docs_pyproject_requirements = ["project.optional-dependencies"]
+# Configuration key for documentation dependencies
+docs_pyproject_requirements = ["project.optional-dependencies"]  # For pyproject.toml
 
 
 # Helper Functions
-def remove_from_file(file_path, content_to_remove):
+def _remove_from_file(file_path: Path, content_to_remove: str) -> None:
+    """Remove specified content from the file.
+
+    Args:
+        file_path (Path): The path of the file from which to remove content.
+        content_to_remove (str): The content to be removed from the file.
+    """
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
@@ -28,7 +37,13 @@ def remove_from_file(file_path, content_to_remove):
         file.writelines(lines)
 
 
-def remove_nested_section(data, nested_key):
+def _remove_nested_section(data: dict, nested_key: str) -> None:
+    """Remove a nested section from a dictionary representing a TOML file.
+
+    Args:
+        data (dict): The dictionary from which to remove the section.
+        nested_key (str): The dotted path key representing the nested section to remove.
+    """
     keys = nested_key.split('.')
     current_data = data
     # Look for Parent section
@@ -51,30 +66,46 @@ def remove_nested_section(data, nested_key):
             break  # If the section is not empty, stop removing
 
 
-def remove_from_toml(file_path, sections_to_remove):
+def _remove_from_toml(file_path: Path, sections_to_remove: list) -> None:
+    """Remove specified sections from a TOML file.
+
+    Args:
+        file_path (Path): The path to the TOML file.
+        sections_to_remove (list): A list of section keys to remove from the TOML file.
+    """
     # Load the TOML file
     with open(file_path, 'r') as file:
         data = toml.load(file)
 
     # Remove the specified sections
     for section in sections_to_remove:
-        remove_nested_section(data, section)
+        _remove_nested_section(data, section)
 
     with open(file_path, 'w') as file:
         toml.dump(data, file)
 
 
-def remove_dir(path):
+def _remove_dir(path: Path) -> None:
+    """Remove a directory if it exists.
+
+    Args:
+        path (Path): The path of the directory to remove.
+    """
     if path.exists():
         shutil.rmtree(str(path))
 
 
-def remove_file(path):
+def _remove_file(path: Path) -> None:
+    """Remove a file if it exists.
+
+    Args:
+        path (Path): The path of the file to remove.
+    """
     if path.exists():
         path.unlink()
 
 
-def handle_starter_setup(selected_add_ons_list: str, python_package_name) -> None:
+def _handle_starter_setup(selected_add_ons_list: str, python_package_name) -> None:
     """Clean up the unnecessary files in the starters template.
 
     Args:
@@ -96,7 +127,7 @@ def handle_starter_setup(selected_add_ons_list: str, python_package_name) -> Non
     parameter_file_patterns = ["parameters_*.yml", "parameters/*.yml"]
     for pattern in parameter_file_patterns:
         for param_file in conf_base_path.glob(pattern):
-            remove_file(param_file)
+            _remove_file(param_file)
 
     # Remove the pipelines subdirectories
     if "Kedro Viz" in selected_add_ons_list: # Remove reporting if Kedro Viz is selected
@@ -106,53 +137,53 @@ def handle_starter_setup(selected_add_ons_list: str, python_package_name) -> Non
 
     pipelines_path = current_dir / f"src/{python_package_name}/pipelines/"
     for pipeline_subdir in pipelines_to_remove:
-        remove_dir(pipelines_path / pipeline_subdir)
+        _remove_dir(pipelines_path / pipeline_subdir)
 
     # Remove all test files from tests/pipelines/
     test_pipeline_path = current_dir / "tests/pipelines/test_data_science.py"
-    remove_file(test_pipeline_path)
+    _remove_file(test_pipeline_path)
 
 
-def setup_template_add_ons(selected_add_ons_list: str, requirements_file_path:str, pyproject_file_path: str, python_package_name: str) -> None:
-    """Setup the starter templates according to the choice of add-ons.
+def setup_template_add_ons(selected_add_ons_list: str, requirements_file_path: str, pyproject_file_path: str, python_package_name: str) -> None:
+    """Setup the templates according to the choice of add-ons.
 
     Args:
         selected_add_ons_list (str): A string contains the selected add-ons.
-        requirements_file_path (str): The path of the `requiremenets.txt` in the starter template.
-        pyproject_file_path (str): The path of the `pyproject.toml` in the starter template
+        requirements_file_path (str): The path of the `requiremenets.txt` in the template.
+        pyproject_file_path (str): The path of the `pyproject.toml` in the template
         python_package_name (str): The name of the python package.
     """
     if "Linting" not in selected_add_ons_list:
-        remove_from_file(requirements_file_path, lint_requirements)
-        remove_from_toml(pyproject_file_path, lint_pyproject_requirements)
+        _remove_from_file(requirements_file_path, lint_requirements)
+        _remove_from_toml(pyproject_file_path, lint_pyproject_requirements)
 
     if "Testing" not in selected_add_ons_list:
-        remove_from_file(requirements_file_path, test_requirements)
-        remove_from_toml(pyproject_file_path, test_pyproject_requirements)
-        remove_dir(current_dir / "tests")
+        _remove_from_file(requirements_file_path, test_requirements)
+        _remove_from_toml(pyproject_file_path, test_pyproject_requirements)
+        _remove_dir(current_dir / "tests")
 
     if "Logging" not in selected_add_ons_list:
-        remove_file(current_dir / "conf/logging.yml")
+        _remove_file(current_dir / "conf/logging.yml")
 
     if "Documentation" not in selected_add_ons_list:
-        remove_from_toml(pyproject_file_path, docs_pyproject_requirements)
-        remove_dir(current_dir / "docs")
+        _remove_from_toml(pyproject_file_path, docs_pyproject_requirements)
+        _remove_dir(current_dir / "docs")
 
     if "Data Structure" not in selected_add_ons_list:
-        remove_dir(current_dir / "data")
+        _remove_dir(current_dir / "data")
 
     if "Pyspark" in selected_add_ons_list:
-        handle_starter_setup(selected_add_ons_list, python_package_name)
+        _handle_starter_setup(selected_add_ons_list, python_package_name)
 
     if "Kedro Viz" in selected_add_ons_list:
-        handle_starter_setup(selected_add_ons_list, python_package_name)
+        _handle_starter_setup(selected_add_ons_list, python_package_name)
 
 
-def sort_requirements(requirements_file_path):
-    """Sort the requirements.txt file in alphabetical order.
+def sort_requirements(requirements_file_path: Path) -> None:
+    """Sort the requirements.txt file alphabetically and write it back to the file.
 
     Args:
-        requirements_file_path: the path to the requirements.txt file.
+        requirements_file_path (Path): The path to the `requirements.txt` file.
     """
     with open(requirements_file_path, 'r') as requirements:
         lines = requirements.readlines()

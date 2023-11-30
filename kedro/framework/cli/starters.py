bf8814e611ab1b0d2ffeb8c58e1a6b780de0f870
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2023-11-30 16:21:27 +0000

    Add suggestion from review
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- kedro/framework/cli/starters.py
+++ kedro/framework/cli/starters.py
@@ -703,23 +703,20 @@ def _validate_config_file_against_prompts(
     """
     if config is None:
         raise KedroCliError("Config file is empty.")
+    additional_keys = {"tools": "none", "example_pipeline": "no"}
     missing_keys = set(prompts) - set(config)
-    missing_mandatory_keys = missing_keys - {"tools", "example_pipeline"}
+    missing_mandatory_keys = missing_keys - set(additional_keys)
     if missing_mandatory_keys:
         click.echo(yaml.dump(config, default_flow_style=False))
         raise KedroCliError(
             f"{', '.join(missing_mandatory_keys)} not found in config file."
         )
-    if "example_pipeline" in missing_keys:
-        click.secho(
-            "The `example_pipeline` key not found in the config file, default value 'no' is being used.",
-            fg="yellow",
-        )
-    if "tools" in missing_keys:
-        click.secho(
-            "The `tools` key not found in the config file, default value 'none' is being used.",
-            fg="yellow",
-        )
+    for key, default_value in additional_keys.items():
+        if key in missing_keys:
+            click.secho(
+                f"The `{key}` key not found in the config file, default value '{default_value}' is being used.",
+                fg="yellow",
+            )
 
     if "output_dir" in config and not Path(config["output_dir"]).exists():
         raise KedroCliError(

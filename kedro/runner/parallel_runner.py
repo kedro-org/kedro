df1f865c9e2300b7461718bedc51fd3df259085c
Merel Theisen <49397448+merelcht@users.noreply.github.com>
2023-12-15 13:49:00 +0000

    Update kedro/runner/parallel_runner.py
    Co-authored-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
    Signed-off-by: Merel Theisen <49397448+merelcht@users.noreply.github.com>

--- kedro/runner/parallel_runner.py
+++ kedro/runner/parallel_runner.py
@@ -258,7 +258,7 @@ class ParallelRunner(AbstractRunner):
         # noqa: import-outside-toplevel,cyclic-import
         if not self._is_async:
             self._logger.info(
-                "Asynchronous mode is disabled for loading and saving data. Use the --async flag "
+                "Using synchronous mode for loading and saving data. Use the --async flag "
                 "for potential performance gains. https://docs.kedro.org/en/stable/nodes_and_pipelines/run_a_pipeline.html#load-and-save-asynchronously"
             )
 

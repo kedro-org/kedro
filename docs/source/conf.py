cf38e2d5d871e3d58108ff67c4f5ee198d464d55
Elena Khaustova <ymax70rus@gmail.com>
2024-04-03 12:32:48 +0100

    Removed odd comment
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -239,7 +239,6 @@ linkcheck_ignore = [
     "https://opensource.org/license/apache2-0-php/",
     "https://docs.github.com/en/rest/overview/other-authentication-methods#via-username-and-password",
     "https://www.educative.io/blog/advanced-yaml-syntax-cheatsheet#anchors",
-    # temporarily, until the file actually exists in `main`
     "https://www.quora.com/What-is-thread-safety-in-Python",  # "403 Client Error: Forbidden for url"
 ]
 

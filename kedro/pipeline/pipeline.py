d024862b9744c0c7b43b3b9db4a63bd6e65a9538
Ankita Katiyar <110245118+ankatiyar@users.noreply.github.com>
2024-04-19 11:34:26 +0100

    Update kedro/pipeline/pipeline.py
    Co-authored-by: Nok Lam Chan <nok.lam.chan@quantumblack.com>
    Signed-off-by: Ankita Katiyar <110245118+ankatiyar@users.noreply.github.com>

--- kedro/pipeline/pipeline.py
+++ kedro/pipeline/pipeline.py
@@ -15,7 +15,8 @@ from graphlib import CycleError, TopologicalSorter
 import kedro
 from kedro.pipeline.node import Node, _to_list
 
-from ._transcoding import TRANSCODING_SEPARATOR, _strip_transcoding  # noqa: F401
+from ._transcoding import _strip_transcoding
+from ._transcoding import TRANSCODING_SEPARATOR  # noqa: F401 for 0.19.x backward compatibility
 
 
 class OutputNotUniqueError(Exception):

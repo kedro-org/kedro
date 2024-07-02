e2908719bb75d7371e3c109d4a94f8b3f7d50475
lrcouto <laurarccouto@gmail.com>
2024-07-02 15:25:39 -0300

    Revert "Update test_logging.py"
    This reverts commit 802ad2607f27c5e68ec11b5ca3dbe5c03995230b.

--- tests/framework/project/test_logging.py
+++ tests/framework/project/test_logging.py
@@ -163,7 +163,7 @@ def test_has_rich_handler():
     assert has_rich_handler(test_logger)
 
 
-def test_default_logging_info_emission(monkeypatch, capsys):
+    def test_default_logging_info_emission(monkeypatch, capsys):
     # Expected path and logging configuration
     expected_path = Path("conf/logging.yml")
     dummy_logging_config = yaml.dump(

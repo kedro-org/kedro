7f3019ffac59cc606618d9284b067407f469a113
Elena Khaustova <ymax70rus@gmail.com>
2024-07-04 19:11:14 +0100

    Replace set with dict to keep original datasets order when printing
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro/io/data_catalog.py
+++ kedro/io/data_catalog.py
@@ -106,7 +106,7 @@ class _FrozenDatasets:
         """Return a _FrozenDatasets instance from some datasets collections.
         Each collection could either be another _FrozenDatasets or a dictionary.
         """
-        self._original_names: set[str] = set()
+        self._original_names: dict[str, str] = {}
         for collection in datasets_collections:
             if isinstance(collection, _FrozenDatasets):
                 self.__dict__.update(collection.__dict__)
@@ -116,7 +116,7 @@ class _FrozenDatasets:
                 # for easy access to transcoded/prefixed datasets.
                 for dataset_name, dataset in collection.items():
                     self.__dict__[_sub_nonword_chars(dataset_name)] = dataset
-                    self._original_names.add(dataset_name)
+                    self._original_names[dataset_name] = ""
 
     # Don't allow users to add/change attributes on the fly
     def __setattr__(self, key: str, value: Any) -> None:
@@ -131,7 +131,7 @@ class _FrozenDatasets:
         raise AttributeError(msg)
 
     def _ipython_key_completions_(self) -> list[str]:
-        return list(self._original_names)
+        return list(self._original_names.keys())
 
     def __getitem__(self, key: str) -> Any:
         return self.__dict__[_sub_nonword_chars(key)]
@@ -139,7 +139,7 @@ class _FrozenDatasets:
     def __repr__(self) -> str:
         datasets_repr = {}
         params_repr = {}
-        for ds_name in self._original_names:
+        for ds_name in self._original_names.keys():
             if ds_name == "parameters" or ds_name.startswith("params:"):
                 params_repr[ds_name] = self.__dict__[
                     _sub_nonword_chars(ds_name)

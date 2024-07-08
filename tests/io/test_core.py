e61a775da58823079393fd7d1c6bd7e470f36b53
Elena Khaustova <ymax70rus@gmail.com>
2024-07-08 18:34:59 +0100

    Updated test_str_representation
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- tests/io/test_core.py
+++ tests/io/test_core.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pprint
 import shutil
 from decimal import Decimal
 from fractions import Fraction
@@ -206,8 +207,12 @@ def dummy_data():
 class TestCoreFunctions:
     @pytest.mark.parametrize("var", [1, True] + FALSE_BUILTINS)
     def test_str_representation(self, var):
-        filepath = "."
-        assert str(MyDataset(var=var)) == f"MyDataset(filepath={filepath}, var={var})"
+        var_str = pprint.pformat(var)
+        filepath_str = pprint.pformat(PurePosixPath("."))
+        assert (
+            str(MyDataset(var=var))
+            == f"tests.io.test_core.MyDataset(filepath={filepath_str}, var={var_str})"
+        )
 
     def test_str_representation_none(self):
         assert str(MyDataset()) == "MyDataset(filepath=.)"

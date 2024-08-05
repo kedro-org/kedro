8d75a8aad8d00b8ab952811fe0bf52baee9db486
Merel Theisen <merel.theisen@quantumblack.com>
2024-08-05 14:35:45 +0100

    Revert test to use before_command_run again to prevent sending heap events
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- tests/framework/cli/hooks/test_manager.py
+++ tests/framework/cli/hooks/test_manager.py
@@ -5,18 +5,16 @@ from kedro.framework.cli.hooks.specs import CLICommandSpecs
 
 
 @pytest.mark.parametrize(
-    "hook_specs,hook_name,hook_params",
-    [(CLICommandSpecs, "after_command_run", ("project_metadata", "command_args"))],
+    "hook_specs,hook_name",
+    [(CLICommandSpecs, "before_command_run")],
 )
-def test_hook_manager_can_call_hooks_defined_in_specs(
-    hook_specs, hook_name, hook_params
-):
+def test_hook_manager_can_call_hooks_defined_in_specs(hook_specs, hook_name):
     """Tests to make sure that the hook manager can call all hooks defined by specs."""
     cli_hook_manager = CLIHooksManager()
     hook = getattr(cli_hook_manager.hook, hook_name)
     assert hook.spec.namespace == hook_specs
-    kwargs = {param: None for param in hook_params}
+    # kedro-telemetry installed by default, this test runs the telemetry before_command_run hook, which
+    # is expecting the command_args to be a list version of the command like ['kedro', 'run']
+    kwargs = {"command_args": ["kedro", "run"], "project_metadata": None}
     result = hook(**kwargs)
-    # since there hasn't been any hook implementation, the result should be empty
-    # but it shouldn't have raised
     assert result == []

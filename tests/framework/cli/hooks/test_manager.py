0713051cb7c4114dee3a54bda852f3b61927f4a6
Merel Theisen <merel.theisen@quantumblack.com>
2024-08-05 13:38:00 +0100

    Swap test to use after_command_run instead of before_command_run
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- tests/framework/cli/hooks/test_manager.py
+++ tests/framework/cli/hooks/test_manager.py
@@ -5,17 +5,18 @@ from kedro.framework.cli.hooks.specs import CLICommandSpecs
 
 
 @pytest.mark.parametrize(
-    "hook_specs,hook_name",
-    [(CLICommandSpecs, "before_command_run")],
+    "hook_specs,hook_name,hook_params",
+    [(CLICommandSpecs, "after_command_run", ("project_metadata", "command_args"))],
 )
-def test_hook_manager_can_call_hooks_defined_in_specs(hook_specs, hook_name):
+def test_hook_manager_can_call_hooks_defined_in_specs(
+    hook_specs, hook_name, hook_params
+):
     """Tests to make sure that the hook manager can call all hooks defined by specs."""
     cli_hook_manager = CLIHooksManager()
     hook = getattr(cli_hook_manager.hook, hook_name)
     assert hook.spec.namespace == hook_specs
-    # kedro-telemetry installed by default, this test runs the telemetry before_command_run hook, which
-    # is expecting the command_args to be a list version of the command like ['kedro', 'run']
-    kwargs = {"command_args": ["kedro", "run"], "project_metadata": None}
+    kwargs = {param: None for param in hook_params}
     result = hook(**kwargs)
-    # since there hasn't been any hook implementation the result should be empty, but it shouldn't raise
+    # since there hasn't been any hook implementation, the result should be empty
+    # but it shouldn't have raised
     assert result == []

85bf720cf129cfce57e0b3baf5af57c8393af652
Elena Khaustova <ymax70rus@gmail.com>
2024-09-05 12:07:35 +0100

    KedroDataCatalog draft
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro/io/__init__.py
+++ kedro/io/__init__.py
@@ -14,6 +14,7 @@ from .core import (
     Version,
 )
 from .data_catalog import DataCatalog
+from .data_catalog_redesign import KedroDataCatalog
 from .lambda_dataset import LambdaDataset
 from .memory_dataset import MemoryDataset
 from .shared_memory_dataset import SharedMemoryDataset
@@ -26,6 +27,7 @@ __all__ = [
     "DatasetAlreadyExistsError",
     "DatasetError",
     "DatasetNotFoundError",
+    "KedroDataCatalog",
     "LambdaDataset",
     "MemoryDataset",
     "SharedMemoryDataset",

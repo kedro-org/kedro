a4ab4c4809d1bb2085ce04ee799520877cbd3cf1
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-10-09 15:31:21 +0100

    add benchmark
    Signed-off-by: Nok Lam Chan <nok.lam.chan@quantumblack.com>

--- benchmarks/benchmark_dummy.py
+++ benchmarks/benchmark_dummy.py
@@ -11,20 +11,22 @@ import yaml
 import pandas as pd
 from kedro.io.memory_dataset import MemoryDataset
 
+
 # Simulate an I/O-bound task
 def io_bound_task(input_data):
     time.sleep(2)  # Simulate an I/O wait (e.g., reading from a file)
     output = input_data
     return output
 
+
 # Simulate a compute-bound task (matrix multiplication)
-def compute_bound_task(input_data: str) -> str:
-    # Simulate a heavy computation (e.g., large matrix multiplication)
-    matrix_size = 1000
-    matrix_a = np.random.rand(matrix_size, matrix_size)
-    matrix_b = np.random.rand(matrix_size, matrix_size)
-    _ = np.dot(matrix_a, matrix_b)  # Matrix multiplication
-    return f"Computed {input_data}"
+def compute_bound_task(input_data) -> str:
+    # Simulate heavy compute that are not using multicore (not pandas/numpy etc)
+    ans = 1
+    for i in range(1, 50000):
+        ans = ans * i
+    return "dummy"
+
 
 def create_data_catalog():
     """
@@ -37,7 +39,7 @@ def create_data_catalog():
     filepath: '{pattern}.csv'
 
 'numpy_{pattern}':
-    type: pickle.PickletDataset
+    type: pickle.PickleDataset
     filepath: '{pattern}.pkl'
 
 '{catch_all_dataset_pattern}':
@@ -49,15 +51,57 @@ def create_data_catalog():
     return catalog
 
 
-def create_io_bound_node(inputs=None, outputs=None,name=None):
+def create_io_bound_node(inputs=None, outputs=None, name=None):
     io_node = node(io_bound_task, inputs=inputs, outputs=outputs, name=name)
     return io_node
+
+
+def create_io_bound_pipeline():
+    dummy_pipeline = pipeline(
+        [
+            create_io_bound_node("dummy_1", "output_1"),
+            create_io_bound_node("dummy_2", "output_2"),
+            create_io_bound_node("dummy_3", "output_3"),
+            create_io_bound_node("dummy_4", "output_4"),
+            create_io_bound_node("dummy_5", "output_5"),
+            create_io_bound_node("dummy_6", "output_6"),
+            create_io_bound_node("dummy_7", "output_7"),
+            create_io_bound_node("dummy_1", "output_8"),
+            create_io_bound_node("dummy_1", "output_9"),
+            create_io_bound_node("dummy_1", "output_10"),
+        ]
+    )
+    return dummy_pipeline
+
+
+def create_compute_bound_node(inputs=None, outputs=None, name=None):
+    io_node = node(compute_bound_task, inputs=inputs, outputs=outputs, name=name)
+    return io_node
+
+
+def create_compute_bound_pipeline():
+    dummy_pipeline = pipeline(
+        [
+            create_compute_bound_node("dummy_1", "numpy_1"),
+            create_compute_bound_node("dummy_2", "numpy_2"),
+            create_compute_bound_node("dummy_3", "numpy_3"),
+            create_compute_bound_node("dummy_4", "numpy_4"),
+            create_compute_bound_node("dummy_5", "numpy_5"),
+            create_compute_bound_node("dummy_6", "numpy_6"),
+            create_compute_bound_node("dummy_7", "numpy_7"),
+            create_compute_bound_node("dummy_1", "numpy_8"),
+            create_compute_bound_node("dummy_1", "numpy_9"),
+            create_compute_bound_node("dummy_1", "numpy_10"),
+        ]
+    )
+    return dummy_pipeline
 
 
 class TimeSuite:
     """
     A dummy benchmark suite to test with asv framework.
     """
+
     def setup(self):
         self.d = {}
         for x in range(500):
@@ -69,15 +113,25 @@ class TimeSuite:
 
 
 if __name__ == "__main__":
-    print("==="*20)
+    print("===" * 20)
+    import time
 
-    dummy_pipeline = pipeline([
-        create_io_bound_node("dummy_1","output_1",name="dummy_1"),
-        create_io_bound_node("dummy_2","output_2",name="dummy_2"),
-        create_io_bound_node("dummy_3","output_3",name="dummy_3"),
-    ])
-    runner = SequentialRunner()
-    catalog = create_data_catalog()
-    runner.run(dummy_pipeline, catalog)
+    result = {}
+    runner_class = {
+        "SequentialRunner": SequentialRunner,
+        "ThreadRunner": ThreadRunner,
+        "ParallelRunner": ParallelRunner,
+    }
+    for name, runner in runner_class.items():
+        start = time.time()
+        print("==" * 20 + name + "==" * 20)
+        catalog = create_data_catalog()
+        test_pipeline = create_compute_bound_pipeline()
+        runner = runner()
+        runner.run(test_pipeline, catalog=catalog)
+        end = time.time()
+        result[name] = end - start
+        print(f"Time spent: {end - start}")
 
     print("===" * 20 + "Done" + "===" * 20)
+    print(result)

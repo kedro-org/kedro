9b14fcb88412ac643f26a7d7c49198690c73f1d6
Nok <nok.lam.chan@quantumblack.com>
2024-10-31 15:25:43 +0000

    update test  & README
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,7 +5,7 @@ import kedro
 
 def test_import_kedro_with_no_official_support_raise_error(mocker):
     """Test importing kedro with python>=3.14 should fail"""
-    mocker.patch("kedro.sys.version_info", (3, 13))
+    mocker.patch("kedro.sys.version_info", (3, 14))
 
     # We use the parent class to avoid issues with `exec_module`
     with pytest.raises(UserWarning) as excinfo:
@@ -16,7 +16,7 @@ def test_import_kedro_with_no_official_support_raise_error(mocker):
 
 def test_import_kedro_with_no_official_support_emits_warning(mocker):
     """Test importing kedro python>=3.14 and controlled warnings should work"""
-    mocker.patch("kedro.sys.version_info", (3, 13))
+    mocker.patch("kedro.sys.version_info", (3, 14))
     mocker.patch("kedro.sys.warnoptions", ["default:Kedro is not yet fully compatible"])
 
     # We use the parent class to avoid issues with `exec_module`

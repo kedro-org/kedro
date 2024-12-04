177d08932147757b13c89eb305f1001de6815bd1
Laura Couto <laurarccouto@gmail.com>
2024-12-04 20:21:18 -0300

    Change example test on default template
    Signed-off-by: Laura Couto <laurarccouto@gmail.com>

---
+++ kedro/templates/project/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/example/__init__.py
@@ -0,0 +1,5 @@
+from .pipeline import create_pipeline
+
+__all__ = ["create_pipeline"]
+
+__version__ = "0.1"
---
+++ kedro/templates/project/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/example/nodes.py
@@ -0,0 +1,5 @@
+def create_data():
+    return [1, 2, 3, 4, 5]
+
+def process_data(data):
+    return [x * 2 for x in data]

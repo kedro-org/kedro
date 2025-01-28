156a0d362eda75d08c6c7e9ea5445b24ed46619a
Elena Khaustova <ymax70rus@gmail.com>
2025-01-28 14:51:29 +0000

    Updated unit tests
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- tests/io/test_kedro_data_catalog.py
+++ tests/io/test_kedro_data_catalog.py
@@ -271,7 +271,9 @@ class TestKedroDataCatalog:
 
     def test_repr_no_type_found(self, data_catalog_from_config):
         del data_catalog_from_config._lazy_datasets["boats"].config["type"]
-        assert data_catalog_from_config.__repr__() == str(data_catalog_from_config)
+        pattern = "'type' is missing from dataset catalog configuration"
+        with pytest.raises(DatasetError, match=re.escape(pattern)):
+            _ = str(data_catalog_from_config)
 
     def test_missing_keys_from_load_versions(self, correct_config):
         """Test load versions include keys missing in the catalog"""

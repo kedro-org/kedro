cfe866284c65e5d681b716f091a0a04d905cf1f6
Elena Khaustova <ymax70rus@gmail.com>
2025-02-12 19:18:44 +0000

    Simplified rank_catalog_factories method
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro/framework/session/catalog.py
+++ kedro/framework/session/catalog.py
@@ -1,13 +1,9 @@
 class CatalogCommandsMixin:
     context = None
 
-    def rank_catalog_factories(self) -> list[str] | str:
+    def rank_catalog_factories(self) -> list[str]:
         """List all dataset factories in the catalog, ranked by priority
         by which they are matched.
         """
 
-        catalog_factories = self.context.catalog.config_resolver.list_patterns()
-        if catalog_factories:
-            return catalog_factories
-        else:
-            return "There are no dataset factories in the catalog."
+        return self.context.catalog.config_resolver.list_patterns()

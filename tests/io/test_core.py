36965df8c6ad30dc2139bb17a51c5e20a8a4d7aa
rashidakanchwala <rashida_kanchwala@mckinsey.com>
2025-03-17 17:03:02 +0000

    lint fixed

--- tests/io/test_core.py
+++ tests/io/test_core.py
@@ -352,16 +352,16 @@ class TestCoreFunctions:
     def test_parse_dataset_definition_with_python_class_type(self):
         config = {"type": MyDataset}
         parse_dataset_definition(config)
-        
+
     def test_dataset_missing_dependencies(self, mocker):
         dataset_name = "LambdaDataset"
 
         def side_effect_function(value):
-            import unknown_package
+            pass
 
         mocker.patch("kedro.io.core.load_obj", side_effect=side_effect_function)
 
-        pattern = "No module named \"unknown_package\""
+        pattern = 'No module named "unknown_package"'
         with pytest.raises(DatasetError, match=pattern):
             parse_dataset_definition({"type": dataset_name})
 

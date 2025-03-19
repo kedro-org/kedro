e96d4c918225d04d4f2bea715fb4b4f4273f9999
Sajid Alam <90610031+SajidAlamQB@users.noreply.github.com>
2025-03-19 10:48:17 +0000

    Add cloud protocol support for `--conf-source` (#4543)
    * Update conf source to accept cloud
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * lint
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * move methods and protocols to utils.py
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Update omegaconf_config.py
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * add tests
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * update docs and release notes
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * lint
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * preserve the protocol
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * add validation
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Update utils.py
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * coverage
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * fix tests
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * avoid breaking change
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Update configuration_basics.md
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * apply code changes from review
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Changes based on review docs
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * add flaky link to linkcheck ignore
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    ---------
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- kedro/config/omegaconf_config.py
+++ kedro/config/omegaconf_config.py
@@ -21,6 +21,7 @@ from yaml.parser import ParserError
 from yaml.scanner import ScannerError
 
 from kedro.config.abstract_config import AbstractConfigLoader, MissingConfigException
+from kedro.utils import CLOUD_PROTOCOLS, HTTP_PROTOCOLS, _parse_filepath
 
 _config_logger = logging.getLogger(__name__)
 
@@ -104,6 +105,8 @@ class OmegaConfigLoader(AbstractConfigLoader):
 
         Args:
             conf_source: Path to use as root directory for loading configuration.
+                This can be a local filesystem path or a remote URL with protocol
+                (e.g., s3://, gs://, etc.)
             env: Environment that will take precedence over base.
             runtime_params: Extra parameters passed to a Kedro run.
             config_patterns: Regex patterns that specify the naming convention for configuration
@@ -147,6 +150,11 @@ class OmegaConfigLoader(AbstractConfigLoader):
 
         # Setup file system and protocol
         self._fs, self._protocol = self._initialise_filesystem_and_protocol(conf_source)
+
+        # Store remote root path if using cloud protocol
+        if self._protocol in CLOUD_PROTOCOLS or self._protocol in HTTP_PROTOCOLS:
+            options = _parse_filepath(conf_source)
+            self._remote_root_path = options["path"]
 
         super().__init__(
             conf_source=conf_source,
@@ -204,7 +212,10 @@ class OmegaConfigLoader(AbstractConfigLoader):
 
         processed_files: set[Path] = set()
         # Load base env config
-        if self._protocol == "file":
+        # Handle remote paths
+        if self._protocol in CLOUD_PROTOCOLS or self._protocol in HTTP_PROTOCOLS:
+            base_path = f"{self._remote_root_path}/{self.base_env}"
+        elif self._protocol == "file":
             base_path = str(Path(self.conf_source) / self.base_env)
         else:
             base_path = str(Path(self._fs.ls("", detail=False)[-1]) / self.base_env)
@@ -229,7 +240,10 @@ class OmegaConfigLoader(AbstractConfigLoader):
         if run_env == self.base_env:
             return config  # type: ignore[no-any-return]
 
-        if self._protocol == "file":
+        # Handle remote paths
+        if self._protocol in CLOUD_PROTOCOLS or self._protocol in HTTP_PROTOCOLS:
+            env_path = f"{self._remote_root_path}/{run_env}"
+        elif self._protocol == "file":
             env_path = str(Path(self.conf_source) / run_env)
         else:
             env_path = str(Path(self._fs.ls("", detail=False)[-1]) / run_env)
@@ -297,8 +311,18 @@ class OmegaConfigLoader(AbstractConfigLoader):
             Resulting configuration dictionary.
 
         """
-
-        if not self._fs.isdir(Path(conf_path).as_posix()):
+        # Handle directory existence check for remote paths
+        if self._protocol in CLOUD_PROTOCOLS or self._protocol in HTTP_PROTOCOLS:
+            try:
+                # Check directory existence in remote paths
+                self._fs.ls(conf_path)
+            except Exception as exc:
+                raise MissingConfigException(
+                    f"Given configuration path either does not exist "
+                    f"or is not a valid directory: {conf_path}. Error: {exc!s}"
+                )
+        # Original check for local paths
+        elif not self._fs.isdir(Path(conf_path).as_posix()):
             raise MissingConfigException(
                 f"Given configuration path either does not exist "
                 f"or is not a valid directory: {conf_path}"
@@ -358,20 +382,31 @@ class OmegaConfigLoader(AbstractConfigLoader):
     def _initialise_filesystem_and_protocol(
         conf_source: str,
     ) -> tuple[fsspec.AbstractFileSystem, str]:
-        """Set up the file system based on the file type detected in conf_source."""
+        """Set up the file system based on the file type or protocol detected in conf_source."""
+        # Force string for regex
+        conf_source = str(conf_source)
+        # Check if it's an archive file
         file_mimetype, _ = mimetypes.guess_type(conf_source)
         if file_mimetype == "application/x-tar":
-            protocol = "tar"
+            return fsspec.filesystem(protocol="tar", fo=conf_source), "tar"
         elif file_mimetype in (
             "application/zip",
             "application/x-zip-compressed",
             "application/zip-compressed",
         ):
-            protocol = "zip"
+            return fsspec.filesystem(protocol="zip", fo=conf_source), "zip"
+
+        # Parse to check for protocol
+        options = _parse_filepath(conf_source)
+        protocol = options["protocol"]
+
+        # Create and return the appropriate filesystem
+        if protocol in HTTP_PROTOCOLS or protocol in CLOUD_PROTOCOLS:
+            # For HTTP and cloud storage protocols, create the appropriate filesystem
+            return fsspec.filesystem(protocol=protocol), protocol
         else:
-            protocol = "file"
-        fs = fsspec.filesystem(protocol=protocol, fo=conf_source)
-        return fs, protocol
+            # Default to local filesystem
+            return fsspec.filesystem(protocol="file", fo=conf_source), "file"
 
     def _merge_configs(
         self,

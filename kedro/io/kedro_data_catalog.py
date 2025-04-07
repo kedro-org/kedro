5395f49140174a0eb4d08373408e6f608fd39ba9
Elena Khaustova <ymax70rus@gmail.com>
2025-04-07 13:35:48 +0100

    Updated setter log message based on ser default dataset
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro/io/kedro_data_catalog.py
+++ kedro/io/kedro_data_catalog.py
@@ -220,8 +220,8 @@ class KedroDataCatalog(CatalogProtocol):
         elif isinstance(value, _LazyDataset):
             self._lazy_datasets[key] = value
         else:
-            # TODO: update based on selected class
-            self._logger.debug(f"Adding input data as a MemoryDataset - {key}")
+            default_dataset = self.runtime_patterns.get("{default}", {}).get("type", "")
+            self._logger.debug(f"Adding input data as a {default_dataset} - {key}")
             self._datasets[key] = MemoryDataset(data=value)  # type: ignore[abstract]
 
     def __len__(self) -> int:

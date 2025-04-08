71601203c9d18406288eb22c25efc65eeae76e0e
Elena Khaustova <ymax70rus@gmail.com>
2025-04-08 14:26:55 +0100

    Tested run_only_missing and fixed annotation
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro/runner/runner.py
+++ kedro/runner/runner.py
@@ -105,9 +105,11 @@ class AbstractRunner(ABC):
         return run_output
 
     def run_only_missing(
-        self, pipeline: Pipeline, catalog: CatalogProtocol, hook_manager: PluginManager
+        self,
+        pipeline: Pipeline,
+        catalog: CatalogProtocol,
+        hook_manager: PluginManager | None = None,
     ) -> dict[str, Any]:
-        # TODO: check if this method makes sense after changes made
         """Run only the missing outputs from the ``Pipeline`` using the
         datasets provided by ``catalog``, and save results back to the
         same objects.

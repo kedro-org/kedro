ce2421684548e6f3c6359827b60727bddaca453b
Elena Khaustova <ymax70rus@gmail.com>
2025-05-13 13:57:44 +0100

    Added type annotations
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- kedro/io/warning_utils.py
+++ kedro/io/warning_utils.py
@@ -1,17 +1,23 @@
 import threading
+from types import TracebackType
 
 # Thread-local flag to support safe concurrent use
-_catalog_internal_context = threading.local()
-_catalog_internal_context.suppress_warning = False
+_catalog_internal_context: threading.local = threading.local()
+_catalog_internal_context.suppress_warning = False  # type: ignore[attr-defined]
 
 
 class suppress_catalog_warning:
-    def __enter__(self):
-        _catalog_internal_context.suppress_warning = True
+    def __enter__(self) -> None:
+        _catalog_internal_context.suppress_warning = True  # type: ignore[attr-defined]
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        _catalog_internal_context.suppress_warning = False
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        _catalog_internal_context.suppress_warning = False  # type: ignore[attr-defined]
 
 
-def is_warning_suppressed():
+def is_warning_suppressed() -> bool:
     return getattr(_catalog_internal_context, "suppress_warning", False)

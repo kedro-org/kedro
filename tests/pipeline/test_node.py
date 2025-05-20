4aa64c7790e6f1fb66d2f6893abb8ff35f3bbe32
Merel Theisen <49397448+merelcht@users.noreply.github.com>
2025-05-20 13:38:48 +0200

    Proposal: Change default node name (#4715)
    * Return real node name in registry describe
    * Use func name for default name, use unique_key for validation, update messages
    * Update name to add hash for uniqness, revert node validation to use name
    * Update test to match regex pattern instead of hardcoded hash
    
    ---------
    
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- tests/pipeline/test_node.py
+++ tests/pipeline/test_node.py
@@ -420,7 +420,7 @@ class TestNames:
     def test_namespaced(self):
         n = node(identity, ["in"], ["out"], namespace="namespace")
         assert str(n) == "identity([in]) -> [out]"
-        assert n.name == "namespace.identity([in]) -> [out]"
+        assert re.match(r"^namespace\.identity__[0-9a-f]{8}$", n.name)
         assert n.short_name == "Identity"
 
     def test_named_and_namespaced(self):
@@ -432,25 +432,25 @@ class TestNames:
     def test_function(self):
         n = node(identity, ["in"], ["out"])
         assert str(n) == "identity([in]) -> [out]"
-        assert n.name == "identity([in]) -> [out]"
+        assert re.match(r"^identity__[0-9a-f]{8}$", n.name)
         assert n.short_name == "Identity"
 
     def test_lambda(self):
         n = node(lambda a: a, ["in"], ["out"])
         assert str(n) == "<lambda>([in]) -> [out]"
-        assert n.name == "<lambda>([in]) -> [out]"
+        assert re.match(r"^<lambda>__[0-9a-f]{8}$", n.name)
         assert n.short_name == "<Lambda>"
 
     def test_partial(self):
         n = node(partial(identity), ["in"], ["out"])
         assert str(n) == "<partial>([in]) -> [out]"
-        assert n.name == "<partial>([in]) -> [out]"
+        assert re.match(r"^partial\(identity\)__[0-9a-f]{8}$", n.name)
         assert n.short_name == "<Partial>"
 
     def test_updated_partial(self):
         n = node(update_wrapper(partial(identity), identity), ["in"], ["out"])
         assert str(n) == "identity([in]) -> [out]"
-        assert n.name == "identity([in]) -> [out]"
+        assert re.match(r"^partial\(identity\)__[0-9a-f]{8}$", n.name)
         assert n.short_name == "Identity"
 
     def test_updated_partial_dict_inputs(self):
@@ -460,7 +460,7 @@ class TestNames:
             ["out"],
         )
         assert str(n) == "biconcat([in2]) -> [out]"
-        assert n.name == "biconcat([in2]) -> [out]"
+        assert re.match(r"^partial\(biconcat\)__[0-9a-f]{8}$", n.name)
         assert n.short_name == "Biconcat"
 
 

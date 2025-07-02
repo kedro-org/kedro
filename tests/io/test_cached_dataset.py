38962998056a26075fa7242612fa85fd4fc7b3ff
Elena Khaustova <ymax70rus@gmail.com>
2025-07-02 10:04:39 +0100

    Fixing coverage
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- tests/io/test_cached_dataset.py
+++ tests/io/test_cached_dataset.py
@@ -153,3 +153,7 @@ class TestCachedDataset:
         mocked_memory_dataset = mocker.patch("kedro.io.cached_dataset.MemoryDataset")
         CachedDataset(MemoryDataset(), copy_mode="assign")
         mocked_memory_dataset.assert_called_once_with(copy_mode="assign")
+
+    def test_describe(self, cached_ds):
+        assert "cache" in cached_ds._describe()
+        assert "dataset" in cached_ds._describe()

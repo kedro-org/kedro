c5449679379a80880ddf29660a50feda8eff63af
Merel Theisen <merel.theisen@quantumblack.com>
2025-07-03 15:49:14 +0200

    Fix lint
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- tests/runner/test_run_validation.py
+++ tests/runner/test_run_validation.py
@@ -1,40 +1,44 @@
 import re
+
 import pytest
-from kedro.pipeline import node, pipeline
-from kedro.runner import SequentialRunner
+
 from kedro.io import DataCatalog, SharedMemoryDataCatalog
-from kedro.runner import SequentialRunner, ThreadRunner, ParallelRunner
+from kedro.pipeline import node, pipeline
+from kedro.runner import ParallelRunner, SequentialRunner, ThreadRunner
 
 
 class TestRunnerInputValidation:
     @pytest.mark.parametrize(
-    "runner_class,catalog_class",
-    [
-        (SequentialRunner, DataCatalog),
-        (ThreadRunner, DataCatalog),
-        (ParallelRunner, SharedMemoryDataCatalog),
-    ],
-)
+        "runner_class,catalog_class",
+        [
+            (SequentialRunner, DataCatalog),
+            (ThreadRunner, DataCatalog),
+            (ParallelRunner, SharedMemoryDataCatalog),
+        ],
+    )
     def test_missing_input_raises_value_error(self, runner_class, catalog_class):
         # Pipeline expects input 'missing_input', which is not in the catalog
-        test_pipeline = pipeline([
-            node(lambda x: x, "missing_input", "output")
-        ])
+        test_pipeline = pipeline([node(lambda x: x, "missing_input", "output")])
         catalog = catalog_class()
         runner = runner_class()
         with pytest.raises(ValueError) as excinfo:
             runner.run(test_pipeline, catalog)
-        assert re.search(r"Pipeline input\(s\) .*missing_input.* not found in the ", str(excinfo.value))
+        assert re.search(
+            r"Pipeline input\(s\) .*missing_input.* not found in the ",
+            str(excinfo.value),
+        )
 
     @pytest.mark.parametrize(
-    "runner_class,catalog_class",
-    [
-        (SequentialRunner, DataCatalog),
-        (ThreadRunner, DataCatalog),
-        (ParallelRunner, SharedMemoryDataCatalog),
-    ],
-)
-    def test_missing_input_because_of_pipeline_namespace_raises_value_error(self, persistent_test_dataset, runner_class, catalog_class):
+        "runner_class,catalog_class",
+        [
+            (SequentialRunner, DataCatalog),
+            (ThreadRunner, DataCatalog),
+            (ParallelRunner, SharedMemoryDataCatalog),
+        ],
+    )
+    def test_missing_input_because_of_pipeline_namespace_raises_value_error(
+        self, persistent_test_dataset, runner_class, catalog_class
+    ):
         catalog = catalog_class(
             {
                 "Input1": persistent_test_dataset(
@@ -49,7 +53,8 @@ class TestRunnerInputValidation:
         my_pipeline = pipeline(
             [
                 node(lambda x: x, inputs="Input1", outputs="Output1", name="node1"),
-            ], namespace="test"
+            ],
+            namespace="test",
         )
 
         runner = runner_class()
@@ -57,4 +62,7 @@ class TestRunnerInputValidation:
         with pytest.raises(ValueError) as excinfo:
             runner.run(my_pipeline, catalog)
 
-        assert f"Pipeline input(s) {{'test.Input1'}} not found in the {catalog_class.__name__}" in str(excinfo.value)
+        assert (
+            f"Pipeline input(s) {{'test.Input1'}} not found in the {catalog_class.__name__}"
+            in str(excinfo.value)
+        )

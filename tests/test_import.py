e138f8e9e288f6d47407f7e83acde63f8c7410d7
rettinghaus <klaus.rettinghaus@gmail.com>
2025-12-09 17:48:40 +0100

    Update Python version check to 3.15
    Signed-off-by: rettinghaus <klaus.rettinghaus@gmail.com>

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,8 +4,8 @@ import kedro
 
 
 def test_import_kedro_with_no_official_support_raise_error(mocker):
-    """Test importing kedro with python>=3.14 should fail"""
-    mocker.patch("kedro.sys.version_info", (3, 14))
+    """Test importing kedro with python>=3.15 should fail"""
+    mocker.patch("kedro.sys.version_info", (3, 15))
 
     # We use the parent class to avoid issues with `exec_module`
     with pytest.raises(UserWarning) as excinfo:
@@ -15,8 +15,8 @@ def test_import_kedro_with_no_official_support_raise_error(mocker):
 
 
 def test_import_kedro_with_no_official_support_emits_warning(mocker):
-    """Test importing kedro python>=3.14 and controlled warnings should work"""
-    mocker.patch("kedro.sys.version_info", (3, 14))
+    """Test importing kedro python>=3.15 and controlled warnings should work"""
+    mocker.patch("kedro.sys.version_info", (3, 15))
     mocker.patch("kedro.sys.warnoptions", ["default:Kedro is not yet fully compatible"])
 
     # We use the parent class to avoid issues with `exec_module`

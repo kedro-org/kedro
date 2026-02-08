2e712c2c7e47222abbc4fcb9a5054f522facf1e1
Laura Couto <laurarccouto@gmail.com>
2026-02-08 14:59:41 -0300

    Explicitly inject robots meta tag
    Signed-off-by: Laura Couto <laurarccouto@gmail.com>

--- docs/javascript/readthedocs.js
+++ docs/javascript/readthedocs.js
@@ -1,7 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function () {
+    // Ensure noindex, nofollow is always present (RTD is inconsistent across projects)
+    let robotsMeta = document.querySelector('meta[name="robots"]');
+
+    if (!robotsMeta) {
+        robotsMeta = document.createElement("meta");
+        robotsMeta.name = "robots";
+        document.head.appendChild(robotsMeta);
+    }
+
+    robotsMeta.content = "noindex, nofollow";
+
     // Trigger Read the Docs' search addon instead of Material MkDocs default
-    document.querySelector(".md-search__input").addEventListener("focus", (e) => {
+    const searchInput = document.querySelector(".md-search__input");
+    if (searchInput) {
+        searchInput.addEventListener("focus", () => {
             const event = new CustomEvent("readthedocs-search-show");
             document.dispatchEvent(event);
         });
-    });
+    }
+});

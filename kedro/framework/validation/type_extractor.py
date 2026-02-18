bce832f9c5f006c6ebe1eec91b96963a63e5ab9b
Sajid Alam <sajid_alam@mckinsey.com>
2026-02-18 15:16:49 +0000

    trigger ci
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- kedro/framework/validation/type_extractor.py
+++ kedro/framework/validation/type_extractor.py
@@ -1,4 +1,4 @@
-"""Type extractor for extracting type hints from various sources."""
+"""Type extractor for extracting type hints from various sources. [to delete]"""
 
 from __future__ import annotations
 

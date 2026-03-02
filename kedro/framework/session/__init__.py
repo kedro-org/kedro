42e047c6d33ccc1cc48f5aa48de6b743ef7d9650
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2026-03-02 18:07:21 +0530

    Separate abstract session
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- kedro/framework/session/__init__.py
+++ kedro/framework/session/__init__.py
@@ -2,6 +2,7 @@
 project lifecycle.
 """
 
+from .abstract_session import AbstractSession
 from .session import KedroSession
 
-__all__ = ["KedroSession"]
+__all__ = ["KedroSession", "AbstractSession"]

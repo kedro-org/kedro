28befd9e2d1f3ae605361db3fd02d7123bdbb9b6
Sajid Alam <sajid_alam@mckinsey.com>
2026-03-10 15:35:09 +0000

    simplify init
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- kedro/validation/__init__.py
+++ kedro/validation/__init__.py
@@ -3,22 +3,10 @@
 from .exceptions import ModelInstantiationError, ParameterValidationError
 from .model_factory import instantiate_model
 from .type_extractor import TypeExtractor
-from .utils import (
-    get_typed_fields,
-    is_pydantic_class,
-    is_pydantic_model,
-    resolve_nested_path,
-    set_nested_value,
-)
 
 __all__ = [
     "ModelInstantiationError",
     "ParameterValidationError",
     "TypeExtractor",
-    "get_typed_fields",
     "instantiate_model",
-    "is_pydantic_class",
-    "is_pydantic_model",
-    "resolve_nested_path",
-    "set_nested_value",
 ]

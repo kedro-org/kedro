a38e03fef9dad81f0d08afb6591fb60e22b695b1
Sajid Alam <90610031+SajidAlamQB@users.noreply.github.com>
2026-03-16 16:32:09 +0000

    Add validation parameter validator (#5422)
    * add validation exceptions and pydantic utils
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * add get typed fields to utils
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Add validation source filters
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Add model factory validation
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Update model_factory.py
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Add validation type_extractor and tests
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * Revert "Update model_factory.py"
    
    This reverts commit 96208dec777f1d0c6a1103407c8d6f1ef0abc9d5.
    
    * Add parameter validator
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * fix test
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * lint
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * delete source_filters
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * update validator and tests
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    * changes based on review
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    
    ---------
    
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>
    Signed-off-by: Sajid Alam <90610031+SajidAlamQB@users.noreply.github.com>

--- kedro/validation/__init__.py
+++ kedro/validation/__init__.py
@@ -2,11 +2,13 @@
 
 from .exceptions import ModelInstantiationError, ParameterValidationError
 from .model_factory import instantiate_model
+from .parameter_validator import ParameterValidator
 from .type_extractor import TypeExtractor
 
 __all__ = [
     "ModelInstantiationError",
     "ParameterValidationError",
+    "ParameterValidator",
     "TypeExtractor",
     "instantiate_model",
 ]
